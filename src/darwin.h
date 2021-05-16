cb5916e251cbb14d428e1a835d16b21d6cb4b58f
heapwolf <paolo@async.ly>
2021-05-16 14:57:44 +0200

    add files

---
+++ src/darwin.h
@@ -0,0 +1,501 @@
+//
+// ====================================================================
+//
+// This implementation uses Cocoa WKWebView backend on macOS. It is
+// written using ObjC runtime and uses WKWebView class as a browser runtime.
+// You should pass "-framework Webkit" flag to the compiler.
+//
+// ====================================================================
+//
+#include <CoreGraphics/CoreGraphics.h>
+#include <objc/objc-runtime.h>
+
+#define NSBackingStoreBuffered 2
+
+#define NSWindowStyl0MaskHUDWindow 1 << 13
+#define NSWindowStyleMaskResizable 8
+#define NSWindowStyleMaskMiniaturizable 4
+#define NSWindowStyleMaskTitled 1
+#define NSTexturedBackgroundWindowMask 1 << 8
+#define NSWindowStyleMaskTexturedBackground 1 << 8
+#define NSWindowStyleMaskUnifiedTitleAndToolbar 1 << 12
+#define NSWindowTitleHidden 1
+#define NSFullSizeContentViewWindowMask 1 << 15
+#define NSWindowStyleMaskClosable 2
+
+#define NSApplicationActivationPolicyRegular 0
+
+#define WKUserScriptInjectionTimeAtDocumentStart 0
+
+namespace webview {
+
+SEL NSSelector(const char *s) {
+  return sel_registerName(s);
+}
+
+id NSClass(const char *s) {
+  return (id) objc_getClass(s);
+}
+
+id NSString (const char *s) {
+  return ((id(*) (id, SEL, const char *)) objc_msgSend)(
+    NSClass("NSString"),
+    NSSelector("stringWithUTF8String:"),
+    s
+  );
+}
+
+class cocoa_wkwebview_engine {
+  public:
+
+  cocoa_wkwebview_engine(bool debug, void *window) {
+    // Application
+    id app = ((id(*)(id, SEL))objc_msgSend)(
+      NSClass("NSApplication"),
+      NSSelector("sharedApplication")
+    );
+
+    ((void (*)(id, SEL, long)) objc_msgSend)(
+      app,
+      NSSelector("setActivationPolicy:"),
+      NSApplicationActivationPolicyRegular
+    );
+
+    // Delegate
+    auto cls = objc_allocateClassPair((Class) NSClass("NSResponder"), "AppDelegate", 0);
+
+    class_addProtocol(cls, objc_getProtocol("NSTouchBarProvider"));
+
+    class_addMethod(
+      cls,
+      NSSelector("applicationShouldTerminateAfterLastWindowClosed:"),
+      (IMP)(+[](id, SEL, id) -> BOOL { return 1; }),
+      "c@:@"
+    );
+
+    class_addMethod(
+      cls,
+      NSSelector("menuItemSelected:"),
+      (IMP)(+[](id self, SEL _cmd, id item) {
+        auto w = (cocoa_wkwebview_engine*) objc_getAssociatedObject(self, "webview");
+        assert(w);
+
+        auto vec = getMenuItemDetails(item);
+        auto title = vec[0];
+        auto state = vec[1];
+        auto parent = vec[2];
+        auto seq = vec[3];
+
+        w->eval("(() => {"
+          "  const detail = {"
+          "    title: '" + title + "',"
+          "    parent: '" + parent + "',"
+          "    state: '" + state + "'"
+          "  };"
+
+          "  if (" + seq + " > 0) {"
+          "    window._ipc[" + seq + "].resolve(detail);"
+          "    delete window._ipc[" + seq + "];"
+          "    return;"
+          "  }"
+
+          "  const event = new window.CustomEvent('menuItemSelected', { detail });"
+          "  window.dispatchEvent(event);"
+          "})()"
+        );
+      }),
+      "v@:@:@:"
+    );
+
+    class_addMethod(
+      cls,
+      NSSelector("themeChangedOnMainThread"),
+      (IMP)(+[](id self) {
+        auto w = (cocoa_wkwebview_engine*) objc_getAssociatedObject(self, "webview");
+        assert(w);
+
+        w->eval("(() => {"
+          "  const event = new window.CustomEvent('themeChanged');"
+          "  window.dispatchEvent(event);"
+          "})()"
+        );
+      }),
+      "v"
+    );
+
+    class_addMethod(
+      cls,
+      NSSelector("userContentController:didReceiveScriptMessage:"),
+      (IMP)(+[](id self, SEL, id, id msg) {
+        auto w = (cocoa_wkwebview_engine*) objc_getAssociatedObject(self, "webview");
+        assert(w);
+
+        w->on_message(((const char* (*)(id, SEL))objc_msgSend)(
+          ((id (*)(id, SEL)) objc_msgSend)(
+            msg,
+            NSSelector("body")
+          ),
+          NSSelector("UTF8String")
+        ));
+      }),
+      "v@:@@"
+    );
+
+    objc_registerClassPair(cls);
+
+    auto delegate = ((id(*)(id, SEL))objc_msgSend)(
+      (id)cls,
+      NSSelector("new")
+    );
+
+    objc_setAssociatedObject(
+      delegate,
+      "webview",
+      (id)this,
+      OBJC_ASSOCIATION_ASSIGN
+    );
+
+    ((void (*)(id, SEL, id))objc_msgSend)(
+      app,
+      sel_registerName("setDelegate:"),
+      delegate
+    );
+
+    addListenerThemeChange(delegate);
+
+    // Main window
+    if (window == nullptr) {
+      m_window = ((id(*)(id, SEL))objc_msgSend)(
+        NSClass("NSWindow"),
+        NSSelector("alloc")
+      );
+      
+      m_window = ((id(*)(id, SEL, CGRect, int, unsigned long, int))objc_msgSend)(
+        m_window,
+        NSSelector("initWithContentRect:styleMask:backing:defer:"),
+        CGRectMake(0, 0, 0, 0),
+        0,
+        NSBackingStoreBuffered,
+        0
+      );
+    } else {
+      m_window = (id)window;
+    }
+
+    // Webview
+    auto config = ((id (*)(id, SEL)) objc_msgSend)(
+      NSClass("WKWebViewConfiguration"),
+      NSSelector("new")
+    );
+
+    m_manager = ((id (*)(id, SEL)) objc_msgSend)(
+      config,
+      NSSelector("userContentController")
+    );
+
+    m_webview = ((id (*)(id, SEL)) objc_msgSend)(
+      NSClass("WKWebView"),
+      NSSelector("alloc")
+    );
+
+    if (debug) {
+      //
+      // [[config preferences] setValue:@YES forKey:@"developerExtrasEnabled"];
+      // 
+      ((id(*)(id, SEL, id, id))objc_msgSend)(
+        ((id(*)(id, SEL))objc_msgSend)(
+          config,
+          NSSelector("preferences")
+        ),
+        NSSelector("setValue:forKey:"),
+        ((id(*)(id, SEL, BOOL))objc_msgSend)(
+          NSClass("NSNumber"),
+          NSSelector("numberWithBool:"),
+          1),
+        NSString("developerExtrasEnabled")
+      );
+    }
+
+    //
+    // [[config preferences] setValue:@YES forKey:@"fullScreenEnabled"];
+    //
+    ((id(*)(id, SEL, id, id))objc_msgSend)(
+      ((id(*)(id, SEL))objc_msgSend)(
+        config,
+        NSSelector("preferences")
+      ),
+      NSSelector("setValue:forKey:"),
+      ((id(*)(id, SEL, BOOL))objc_msgSend)(
+        NSClass("NSNumber"),
+        NSSelector("numberWithBool:"),
+        1
+      ),
+      NSString("fullScreenEnabled")
+    );
+
+    //
+    // [[config preferences] setValue:@YES forKey:@"allowFileAccessFromFileURLs"];
+    //
+    ((id(*)(id, SEL, id, id))objc_msgSend)(
+      ((id(*)(id, SEL))objc_msgSend)(
+        config,
+        NSSelector("preferences")
+      ),
+      NSSelector("setValue:forKey:"),
+      ((id(*)(id, SEL, BOOL))objc_msgSend)(
+        NSClass("NSNumber"),
+        NSSelector("numberWithBool:"),
+        1
+      ),
+      NSString("allowFileAccessFromFileURLs")
+    );
+
+    //
+    // [[config preferences] setValue:@YES forKey:@"javaScriptCanAccessClipboard"];
+    //
+    ((id(*)(id, SEL, id, id))objc_msgSend)(
+      ((id(*)(id, SEL))objc_msgSend)(
+        config,
+        NSSelector("preferences")
+      ),
+      NSSelector("setValue:forKey:"),
+      ((id(*)(id, SEL, BOOL))objc_msgSend)(
+        NSClass("NSNumber"),
+        NSSelector("numberWithBool:"),
+        1
+      ),
+      NSString("javaScriptCanAccessClipboard")
+    );
+
+    //
+    // [[config preferences] setValue:@YES forKey:@"DOMPasteAllowed"];
+    //
+    ((id(*)(id, SEL, id, id))objc_msgSend)(
+      ((id(*)(id, SEL))objc_msgSend)(
+        config,
+        NSSelector("preferences")
+      ),
+      NSSelector("setValue:forKey:"),
+      ((id(*)(id, SEL, BOOL))objc_msgSend)(
+        NSClass("NSNumber"),
+        NSSelector("numberWithBool:"),
+        1
+      ),
+      NSString("DOMPasteAllowed")
+    );
+
+    ((void (*)(id, SEL, CGRect, id))objc_msgSend)(
+      m_webview,
+      NSSelector("initWithFrame:configuration:"),
+      CGRectMake(0, 0, 0, 0),
+      config
+    );
+
+    // initTitleBar();
+
+    ((void (*)(id, SEL, id, id))objc_msgSend)(
+      m_manager,
+      NSSelector("addScriptMessageHandler:name:"),
+      delegate,
+      NSString("external")
+    );
+
+    init(R"script(
+      window.external = {
+        invoke: s => {
+          window.webkit.messageHandlers.external.postMessage(s)
+        }
+      }
+     )script");
+
+    ((void (*)(id, SEL, id))objc_msgSend)(
+      m_window,
+      NSSelector("setContentView:"),
+      m_webview
+    );
+
+    ((void (*)(id, SEL, id))objc_msgSend)(
+      m_window,
+      NSSelector("makeKeyAndOrderFront:"),
+      nullptr
+    );
+  }
+
+  ~cocoa_wkwebview_engine() { close(); }
+  void *window() { return (void *)m_window; }
+
+  void terminate() {
+    close();
+
+    ((void (*)(id, SEL, id))objc_msgSend)(
+      NSClass("NSApp"),
+      NSSelector("terminate:"),
+      nullptr
+    );
+  }
+
+  void run() {
+    id app = ((id(*)(id, SEL))objc_msgSend)(
+      NSClass("NSApplication"),
+      NSSelector("sharedApplication")
+    );
+
+    dispatch([&]() {
+      ((void (*)(id, SEL, BOOL))objc_msgSend)(
+        app,
+        NSSelector("activateIgnoringOtherApps:"),
+        1
+      );
+    });
+
+    ((void (*)(id, SEL))objc_msgSend)(app, NSSelector("run"));
+  }
+
+  void dispatch(std::function<void()> f) {
+    dispatch_async_f(dispatch_get_main_queue(), new dispatch_fn_t(f),
+     (dispatch_function_t)([](void *arg) {
+       auto f = static_cast<dispatch_fn_t *>(arg);
+       (*f)();
+       delete f;
+     })
+    );
+  }
+
+  void set_title(const std::string title) {
+    ((void (*)(id, SEL, id))objc_msgSend)(
+      m_window,
+      NSSelector("setTitle:"),
+      ((id(*)(id, SEL, const char *))objc_msgSend)(
+        NSClass("NSString"),
+        NSSelector("stringWithUTF8String:"),
+        title.c_str()
+      )
+    );
+
+    setTitle(m_window);
+  }
+
+  void set_size(int width, int height, int hints) {
+    auto style = 
+      NSWindowStyleMaskClosable |
+      NSWindowStyleMaskTitled |
+      // NSFullSizeContentViewWindowMask |
+      // NSTexturedBackgroundWindowMask |
+      // NSWindowStyleMaskTexturedBackground |
+      // NSWindowTitleHidden |
+      NSWindowStyleMaskMiniaturizable;
+
+    if (hints != WEBVIEW_HINT_FIXED) {
+      style = style | NSWindowStyleMaskResizable;
+    }
+
+    ((void (*)(id, SEL, unsigned long)) objc_msgSend)(
+      m_window,
+      NSSelector("setStyleMask:"),
+      style
+    );
+
+    if (hints == WEBVIEW_HINT_MIN) {
+      ((void (*)(id, SEL, CGSize)) objc_msgSend)(
+        m_window,
+        NSSelector("setContentMinSize:"),
+        CGSizeMake(width, height)
+      );
+    } else if (hints == WEBVIEW_HINT_MAX) {
+      ((void (*)(id, SEL, CGSize)) objc_msgSend)(
+        m_window,
+        NSSelector("setContentMaxSize:"),
+        CGSizeMake(width, height)
+      );
+    } else {
+      ((void (*)(id, SEL, CGRect, BOOL, BOOL)) objc_msgSend)(
+        m_window,
+        NSSelector("setFrame:display:animate:"),
+        CGRectMake(0, 0, width, height),
+        1,
+        0
+      );
+    }
+
+    ((void (*)(id, SEL))objc_msgSend)(m_window, NSSelector("center"));
+    ((void (*)(id, SEL))objc_msgSend)(m_window, NSSelector("setHasShadow:"));
+
+    // setWindowColor(m_window, 0.1, 0.1, 0.1, 0.0);
+
+    ((void (*)(id, SEL, BOOL))objc_msgSend)(m_window, NSSelector("setTitlebarAppearsTransparent:"), 1);
+    ((void (*)(id, SEL, BOOL))objc_msgSend)(m_window, NSSelector("setOpaque:"), 1);
+
+    // ((void (*)(id, SEL, BOOL))objc_msgSend)(m_window, "setTitleVisibility:"_sel, 1);
+    // ((void (*)(id, SEL, BOOL))objc_msgSend)(m_window, "setMovableByWindowBackground:"_sel, 1);
+    // setWindowButtonsOffset:NSMakePoint(12, 10)
+    // setTitleVisibility:NSWindowTitleHidden
+  }
+
+  void navigate(const std::string url) {
+    auto nsurl = ((id(*)(id, SEL, id))objc_msgSend)(
+      NSClass("NSURL"),
+      NSSelector("URLWithString:"),
+      ((id(*)(id, SEL, const char *))objc_msgSend)(
+        NSClass("NSString"),
+        NSSelector("stringWithUTF8String:"),
+        url.c_str()
+      )
+    );
+
+    ((void (*)(id, SEL, id))objc_msgSend)(
+      m_webview,
+      NSSelector("loadRequest:"),
+      ((id(*)(id, SEL, id))objc_msgSend)(
+        NSClass("NSURLRequest"),
+        NSSelector("requestWithURL:"),
+        nsurl
+      )
+    );
+  }
+
+  void init(const std::string js) {
+    // Equivalent Obj-C:
+    // [m_manager addUserScript:[[WKUserScript alloc] initWithSource:[NSString stringWithUTF8String:js.c_str()] injectionTime:WKUserScriptInjectionTimeAtDocumentStart forMainFrameOnly:YES]]
+    ((void (*)(id, SEL, id)) objc_msgSend)(
+      m_manager,
+      NSSelector("addUserScript:"),
+      ((id (*)(id, SEL, id, long, BOOL)) objc_msgSend)(
+        ((id (*)(id, SEL)) objc_msgSend)(
+          NSClass("WKUserScript"),
+          NSSelector("alloc")
+        ),
+        NSSelector("initWithSource:injectionTime:forMainFrameOnly:"),
+        ((id (*)(id, SEL, const char *)) objc_msgSend)(
+          NSClass("NSString"),
+          NSSelector("stringWithUTF8String:"),
+          js.c_str()
+        ),
+        WKUserScriptInjectionTimeAtDocumentStart,
+        1
+      )
+    );
+  }
+
+  void eval(const std::string js) {
+    ((void (*)(id, SEL, id, id)) objc_msgSend)(
+      m_webview,
+      NSSelector("evaluateJavaScript:completionHandler:"),
+      ((id(*)(id, SEL, const char *)) objc_msgSend)(
+        NSClass("NSString"),
+        NSSelector("stringWithUTF8String:"),
+        js.c_str()
+      ),
+      nullptr
+    );
+  }
+
+private:
+  virtual void on_message(const std::string msg) = 0;
+  void close() { ((void (*)(id, SEL))objc_msgSend)(m_window, NSSelector("close")); }
+  id m_window;
+  id m_webview;
+  id m_manager;
+};
+
+using browser_engine = cocoa_wkwebview_engine;
+} // namespace webview
---
+++ src/linux.h
@@ -0,0 +1,203 @@
+//
+// ====================================================================
+//
+// This implementation uses webkit2gtk backend. It requires gtk+3.0 and
+// webkit2gtk-4.0 libraries. Proper compiler flags can be retrieved via:
+//
+//   pkg-config --cflags --libs gtk+-3.0 webkit2gtk-4.0
+//
+// ====================================================================
+//
+#include <JavaScriptCore/JavaScript.h>
+#include <gtk/gtk.h>
+#include <webkit2/webkit2.h>
+
+namespace webview {
+
+class gtk_webkit_engine {
+  public:
+  gtk_webkit_engine(bool debug, void *window)
+      : m_window(static_cast<GtkWidget *>(window)) {
+
+    gtk_init_check(0, NULL);
+    m_window = static_cast<GtkWidget *>(window);
+
+    if (m_window == nullptr) {
+      m_window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    }
+
+    g_signal_connect(G_OBJECT(m_window), "destroy",
+      G_CALLBACK(+[](GtkWidget *, gpointer arg) {
+        static_cast<gtk_webkit_engine *>(arg)->terminate();
+        exit(0);
+      }),
+      this
+    );
+
+    // Initialize webview widget
+    m_webview = webkit_web_view_new();
+
+    WebKitUserContentManager *manager =
+      webkit_web_view_get_user_content_manager(WEBKIT_WEB_VIEW(m_webview));
+
+    g_signal_connect(
+      manager,
+      "script-message-received::external",
+      G_CALLBACK(+[](WebKitUserContentManager *,
+      WebKitJavascriptResult *r, gpointer arg) {
+        auto *w = static_cast<gtk_webkit_engine *>(arg);
+#if WEBKIT_MAJOR_VERSION >= 2 && WEBKIT_MINOR_VERSION >= 22
+        JSCValue *value =
+          webkit_javascript_result_get_js_value(r);
+        char *s = jsc_value_to_string(value);
+#else
+        JSGlobalContextRef ctx =
+          webkit_javascript_result_get_global_context(r);
+        JSValueRef value = webkit_javascript_result_get_value(r);
+        JSStringRef js = JSValueToStringCopy(ctx, value, NULL);
+        size_t n = JSStringGetMaximumUTF8CStringSize(js);
+        char *s = g_new(char, n);
+        JSStringGetUTF8CString(js, s, n);
+        JSStringRelease(js);
+#endif
+        w->on_message(s);
+        g_free(s);
+      }),
+      this
+    );
+
+    webkit_user_content_manager_register_script_message_handler(
+      manager,
+      "external"
+    );
+
+    init(
+      "window.external = {"
+      "  invoke: s => window.webkit.messageHandlers.external.postMessage(s)"
+      "}"
+    );
+
+    m_vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
+    gtk_container_add(GTK_CONTAINER (m_window), m_vbox);
+
+    // add the webview to the vertical box
+    // gtk_container_add(GTK_CONTAINER(m_window), GTK_WIDGET(m_webview));
+    gtk_widget_grab_focus(GTK_WIDGET(m_webview));
+
+    WebKitSettings *settings =
+        webkit_web_view_get_settings(WEBKIT_WEB_VIEW(m_webview));
+    webkit_settings_set_javascript_can_access_clipboard(settings, true);
+
+    if (debug) {
+      webkit_settings_set_enable_write_console_messages_to_stdout(settings, true);
+      webkit_settings_set_enable_developer_extras(settings, true);
+    }
+
+    gtk_box_pack_end(GTK_BOX (m_vbox), m_webview, TRUE, TRUE, 0);
+    gtk_widget_show_all(m_window);
+  }
+
+  void menu(const std::string menu) {
+    GtkWidget *menubar;
+    GtkWidget *fileMenu;
+    GtkWidget *fileMi;
+    GtkWidget *quitMi;
+
+    menubar = gtk_menu_bar_new();
+    fileMenu = gtk_menu_new();
+
+    fileMi = gtk_menu_item_new_with_label("File");
+    quitMi = gtk_menu_item_new_with_label("Quit");
+
+    gtk_menu_item_set_submenu(GTK_MENU_ITEM(fileMi), fileMenu);
+    gtk_menu_shell_append(GTK_MENU_SHELL(fileMenu), quitMi);
+    gtk_menu_shell_append(GTK_MENU_SHELL(menubar), fileMi);
+
+    gtk_box_pack_start(GTK_BOX(m_vbox), menubar, FALSE, FALSE, 0);
+
+    g_signal_connect(G_OBJECT(quitMi), "activate",
+      G_CALLBACK(+[](GtkWidget *, gpointer arg) {
+        static_cast<webview::gtk_webkit_engine*>(arg)->terminate();
+        exit(0);
+      }),
+      this
+    );
+    
+    gtk_widget_show_all(this->m_window);
+  }
+
+  void *window() { return (void *)m_window; }
+
+  void run() { gtk_main(); }
+
+  void terminate() { gtk_main_quit(); }
+
+  void dispatch(std::function<void()> f) {
+    g_idle_add_full(
+      G_PRIORITY_HIGH_IDLE,
+      (GSourceFunc)([](void *f) -> int {
+        (*static_cast<dispatch_fn_t *>(f))();
+        return G_SOURCE_REMOVE;
+      }),
+    new std::function<void()>(f),
+    [](void *f) { delete static_cast<dispatch_fn_t *>(f); }
+    );
+  }
+
+  void set_title(const std::string title) {
+    gtk_window_set_title(GTK_WINDOW(m_window), title.c_str());
+  }
+
+  void set_size(int width, int height, int hints) {
+    gtk_window_set_resizable(
+      GTK_WINDOW(m_window),
+      hints != WEBVIEW_HINT_FIXED
+    );
+
+    if (hints == WEBVIEW_HINT_NONE) {
+      gtk_window_resize(GTK_WINDOW(m_window), width, height);
+    } else if (hints == WEBVIEW_HINT_FIXED) {
+      gtk_widget_set_size_request(m_window, width, height);
+    } else {
+      GdkGeometry g;
+      g.min_width = g.max_width = width;
+      g.min_height = g.max_height = height;
+      GdkWindowHints h =
+          (hints == WEBVIEW_HINT_MIN ? GDK_HINT_MIN_SIZE : GDK_HINT_MAX_SIZE);
+      // This defines either MIN_SIZE, or MAX_SIZE, but not both:
+      gtk_window_set_geometry_hints(GTK_WINDOW(m_window), nullptr, &g, h);
+    }
+  }
+
+  void navigate(const std::string url) {
+    webkit_web_view_load_uri(WEBKIT_WEB_VIEW(m_webview), url.c_str());
+  }
+
+  void init(const std::string js) {
+    WebKitUserContentManager *manager =
+        webkit_web_view_get_user_content_manager(WEBKIT_WEB_VIEW(m_webview));
+    webkit_user_content_manager_add_script(
+        manager, webkit_user_script_new(
+                     js.c_str(), WEBKIT_USER_CONTENT_INJECT_TOP_FRAME,
+                     WEBKIT_USER_SCRIPT_INJECT_AT_DOCUMENT_START, NULL, NULL));
+  }
+
+  void eval(const std::string js) {
+    webkit_web_view_run_javascript(
+      WEBKIT_WEB_VIEW(m_webview),
+      js.c_str(),
+      NULL,
+      NULL,
+      NULL
+    );
+  }
+
+  GtkWidget *m_vbox;
+  GtkWidget *m_window;
+private:
+  virtual void on_message(const std::string msg) = 0;
+  GtkWidget *m_webview;
+};
+
+using browser_engine = gtk_webkit_engine;
+} // namespace webview
