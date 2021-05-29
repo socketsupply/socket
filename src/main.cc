933e1eae46f93ccc6fca477352f1ff53aaaf6a07
heapwolf <paolo@async.ly>
2021-05-29 12:38:25 +0200

    remove log

--- src/main.cc
+++ src/main.cc
@@ -46,7 +46,6 @@ int main(int argc, char *argv[])
     settings["cmd"],
     cwd,
     [&](Opkit::Process::string_type stdout) {
-      std::cout << "[" << stdout << "]";
       while (!isDocumentReady) {
         std::this_thread::sleep_for(std::chrono::milliseconds(16));
       }
