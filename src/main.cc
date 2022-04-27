90746d6d418a1e1daca3158bdffb85ff7b4a9e74
heapwolf <paolo@async.ly>
2022-04-27 13:01:44 +0200

    add a default menu and check for cmd when starting in dev mode

--- src/main.cc
+++ src/main.cc
@@ -228,12 +228,12 @@ MAIN {
 
     w0.show("");
     w0.setSize("", 1024, 720, 0);
+  }
 
-    auto path = fs::path(cwd) / "index.html";
-    auto file = "file://" + path.string();
-    auto url = "http://localhost:" + std::to_string(_port);
-
-    w0.navigate("", _port > 0 ? url : file);
+  if (_port > 0) {
+    w0.navigate("", "http://localhost:" + std::to_string(_port));
+  } else if (cmd.size() == 0) {
+    w0.navigate("", "file://" + (fs::path(cwd) / "index.html").string());
   }
 
   if (_port > 0) {
