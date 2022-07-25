2bf6f171f8b9d78e11ff47b9186c7f85ef64578e
heapwolf <paolo@socketsupply.co>
2022-07-25 10:17:25 +0200

    add test

---
+++ test/smoke.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+function quiet () {
+  "$@" > /dev/null 2>&1
+}
+
+function die {
+  if [ ! $1 = 0 ]; then
+    echo "not ok - $2" && exit 1
+  fi
+  echo "ok - $2"
+}
+
+echo "TAP version 13"
+echo "1..N"
+
+TEST=true ./bin/install.sh ios
+die $? "the cli tool was built"
+
+mkdir -p test/tmp
+cd test/tmp
+
+echo # the init command creates dirs and ouputs files
+
+../../bin/cli init
+
+quiet stat src/index.html
+die $? "the index.html file exists"
+
+quiet stat ssc.config
+die $? "the config file exists"
+
+quiet ../../bin/cli compile .
+die $? "the compile command executed"
+
+#
+# MacOS
+#
+if [ "`uname -s`" = "Darwin"]; then
+  quiet stat build/beepboop-dev.app/Contents/MacOS/boop-dev
+  die $? "the compile command created a binary in the correct location"
+
+  osascript -l JavaScript <<EOD
+  'use strict'
+
+  function run (args) {
+    var app = new Application("build/beepboop-dev.app/Contents/MacOS/boop-dev");
+    app.activate();
+    var window = app.windows[0];
+    console.log(window);
+  }
+  EOD
+fi
+
+#
+# Linux
+#
+if [ "`uname -s`" = "linux"]; then
+  echo "TODO"
+  exit 1
+fi
+
+#
+# Windows (via WSL)
+#
+if [[ "`uname -a`" == *"$indows"* ]]; then
+  echo "TODO"
+  exit 1
+fi
+
+cd ../..
+rm -rf test/tmp
