c7d5a84e679da31b2b64bd72880e3caaa4da7d46
Joseph Werle <joseph.werle@gmail.com>
2022-10-03 23:29:08 -0400

    refactor(window): revert move lib defs

--- src/window/win.cc
+++ src/window/win.cc
@@ -1,18 +1,5 @@
 #include "window.hh"
 #include <wrl.h>
-#if defined(_WIN32)
-#include <WebView2.h>
-#include <WebView2Experimental.h>
-#include <WebView2EnvironmentOptions.h>
-#include <WebView2ExperimentalEnvironmentOptions.h>
-
-#pragma comment(lib, "advapi32.lib")
-#pragma comment(lib, "shell32.lib")
-#pragma comment(lib, "version.lib")
-#pragma comment(lib, "user32.lib")
-#pragma comment(lib, "WebView2LoaderStatic.lib")
-#pragma comment(lib, "uv_a.lib")
-#endif
 
 #ifndef CHECK_FAILURE
 #define CHECK_FAILURE(...)
