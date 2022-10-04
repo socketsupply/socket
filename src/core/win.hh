12ef8602ed3822ae81eef2d3a2e5cac5d275d34d
Joseph Werle <joseph.werle@gmail.com>
2022-10-03 22:04:27 -0400

    fix(core/win.hh): add headers back

--- src/core/win.hh
+++ src/core/win.hh
@@ -2,7 +2,9 @@
 #define SSC_CORE_WIN_H
 #if defined(_WIN32)
 #include <WebView2.h>
+#include <WebView2Experimental.h>
 #include <WebView2EnvironmentOptions.h>
+#include <WebView2ExperimentalEnvironmentOptions.h>
 
 #pragma comment(lib, "advapi32.lib")
 #pragma comment(lib, "shell32.lib")
