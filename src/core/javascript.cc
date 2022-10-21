7283356cca81a9f648ef6140cfc68bdffa404de4
Joseph Werle <joseph.werle@gmail.com>
2022-10-21 23:10:30 +0200

    refactore(common,core,ipc,window): clean up, missing js, etc

--- src/core/javascript.cc
+++ src/core/javascript.cc
@@ -1,4 +1,5 @@
 #include "core.hh"
+#include "json.hh"
 
 namespace SSC {
   String createJavaScript (const String& name, const String& source) {
@@ -12,10 +13,37 @@ namespace SSC {
     const String& event,
     const String& value
   ) {
+    return getEmitToRenderProcessJavaScript(event, value, "window", JSON::Object {});
+  }
+
+  String getEmitToRenderProcessJavaScript (
+    const String& event,
+    const String& value,
+    const String& target,
+    const JSON::Object& options
+  ) {
     return createJavaScript("emit-to-render-process.js",
-      "const name = decodeURIComponent(`" + event + "`); \n"
-      "const value = `" + value + "`;                    \n"
-      "window._ipc.emit(name, value);                    \n"
+      "const name = decodeURIComponent(`" + event + "`);                   \n"
+      "const value = `" + value + "`;                                      \n"
+      "const target = " + target + ";                                      \n"
+      "const options = " + options.str() + ";                              \n"
+      "let detail = value;                                                 \n"
+      "                                                                    \n"
+      "if (typeof value === 'string') {                                    \n"
+      "  try {                                                             \n"
+      "    detail = decodeURIComponent(value);                             \n"
+      "    detail = JSON.parse(detail);                                    \n"
+      "  } catch (err) {                                                   \n"
+      "    if (!detail) {                                                  \n"
+      "      console.error(`${err.message} (${value})`);                   \n"
+      "      return;                                                       \n"
+      "    }                                                               \n"
+      "  }                                                                 \n"
+      "}                                                                   \n"
+      "                                                                    \n"
+      "const event = new window.CustomEvent(name, { detail, ...options }); \n"
+      "target.dispatchEvent(event);                                        \n"
+    "}                                                                     \n"
     );
   }
 
@@ -50,11 +78,41 @@ namespace SSC {
     const String& state,
     const String& value
   ) {
-    return createJavaScript("resolve-to-render-proecss.js",
-      "const seq = String('" + seq + "');      \n"
-      "const state = Number('" + state + "');  \n"
-      "const value = '" + value + "';          \n"
-      "window._ipc.resolve(seq, state, value);"
+    return createJavaScript("resolve-to-render-process.js",
+      "const seq = String('" + seq + "');                    \n"
+      "const value = '" + value + "';                        \n"
+      "const index = window.__args.index;                    \n"
+      "const state = Number('" + state + "');                \n"
+      "const eventName = `resolve-${index}-${seq}`;          \n"
+      "let detail = value;                                   \n"
+      "                                                      \n"
+      "if (typeof value === 'string') {                      \n"
+      "  try {                                               \n"
+      "    detail = decodeURIComponent(value);               \n"
+      "    detail = JSON.parse(detail);                      \n"
+      "  } catch (err) {                                     \n"
+      "    if (!detail) {                                    \n"
+      "      console.error(`${err.message} (${value})`);     \n"
+      "      return;                                         \n"
+      "    }                                                 \n"
+      "  }                                                   \n"
+      "}                                                     \n"
+      "                                                      \n"
+      "if (detail?.err) {                                    \n"
+      "  let err = detail?.err ?? detail;                    \n"
+      "  if (typeof err === 'string') {                      \n"
+      "    err = new Error(err);                             \n"
+      "  }                                                   \n"
+      "                                                      \n"
+      "  detail = { err };                                   \n"
+      "} else if (detail?.data) {                            \n"
+      "  detail = { ...detail }                              \n"
+      "} else {                                              \n"
+      "  detail = { data: detail }                           \n"
+      "}                                                     \n"
+      "                                                      \n"
+      "const event = new CustomEvent(eventName, { detail }); \n"
+      "window.dispatchEvent(event);                          \n"
     );
   }
 }
