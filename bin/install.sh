c6ac97cbfac31dee7f27ce7a9b94f90900d686d8
Sergey Rubanov <chi187@gmail.com>
2022-11-16 02:26:16 +0400

    wrap build command into new Process

--- bin/install.sh
+++ bin/install.sh
@@ -83,7 +83,7 @@ function _build {
   die $? "not ok - unable to build. See trouble shooting guide in the README.md file"
 
   echo "# building cli for desktop (`uname -m`)..."
-  "$CXX" src/cli/cli.cc ${CXX_FLAGS} ${CXXFLAGS} \
+  "$CXX" src/cli/cli.cc src/process/unix.cc ${CXX_FLAGS} ${CXXFLAGS} \
     -o bin/cli \
     -std=c++2a \
     -DSSC_BUILD_TIME="$(date '+%s')" \
