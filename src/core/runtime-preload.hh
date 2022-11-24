f407b4c632eefcbcb5820d1d96e8d5673734d339
Sergey Rubanov <chi187@gmail.com>
2022-11-24 18:40:55 +0400

    rename platform to os

--- src/core/runtime-preload.hh
+++ src/core/runtime-preload.hh
@@ -16,7 +16,7 @@ namespace SSC {
       "    value: '" + platform.arch + "',                                       \n"
       "    enumerable: true                                                      \n"
       "  },                                                                      \n"
-      "  platform: {                                                             \n"
+      "  os: {                                                             \n"
       "    value: '" + platform.os + "',                                         \n"
       "    enumerable: true                                                      \n"
       "  },                                                                      \n"
