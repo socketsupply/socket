f53ecded75bd16eccf90ee6f1aafd9a397c209e2
Joseph Werle <joseph.werle@gmail.com>
2022-12-07 01:03:34 +0100

    refactor(android): runtime lifecycles, clean up

--- src/android/runtime.kt
+++ src/android/runtime.kt
@@ -6,12 +6,16 @@ interface IRuntimeConfiguration {
   val rootDirectory: String
   val assetManager: android.content.res.AssetManager
   val evaluateJavascript: (String) -> Unit
+  val exit: (Int) -> Unit
+  val openExternal: (String) -> Unit
 }
 
 data class RuntimeConfiguration (
   override val rootDirectory: String,
   override val assetManager: android.content.res.AssetManager,
-  override val evaluateJavascript: (String) -> Unit
+  override val evaluateJavascript: (String) -> Unit,
+  override val exit: (Int) -> Unit,
+  override val openExternal: (String) -> Unit
 ) : IRuntimeConfiguration
 
 open class Runtime (
@@ -30,6 +34,31 @@ open class Runtime (
     this.pointer = 0
   }
 
+  public fun evaluateJavascript (source: String) {
+    this.configuration.evaluateJavascript(source)
+  }
+
+  public fun exit (code: Int) {
+    this.configuration.exit(code)
+  }
+
+  public fun openExternal (value: String) {
+    this.configuration.openExternal(value)
+  }
+
+  public fun start () {
+    this.resume()
+  }
+
+  public fun stop () {
+    this.pause()
+  }
+
+  public fun destroy () {
+    this.stop()
+    this.finalize()
+  }
+
   @Throws(java.lang.Exception::class)
   external fun alloc (rootDirectory: String): Long;
 
@@ -37,4 +66,22 @@ open class Runtime (
   external fun dealloc (): Boolean;
 
   external fun isDebugEnabled (): Boolean;
+
+  @Throws(java.lang.Exception::class)
+  external fun pause (): Boolean;
+
+  @Throws(java.lang.Exception::class)
+  external fun resume (): Boolean;
+
+  @Throws(java.lang.Exception::class)
+  external fun startEventLoop (): Boolean;
+
+  @Throws(java.lang.Exception::class)
+  external fun stopEventLoop (): Boolean;
+
+  @Throws(java.lang.Exception::class)
+  external fun startTimers (): Boolean;
+
+  @Throws(java.lang.Exception::class)
+  external fun stopTimers (): Boolean;
 }
