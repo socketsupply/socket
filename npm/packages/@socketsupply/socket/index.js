c9b95833d08c4769b5d8c95db43785828e7af502
Joseph Werle <joseph.werle@gmail.com>
2023-01-29 16:30:47 +0100

    refactor(npm): clean up and prepare modules for usage in node

--- npm/packages/@socketsupply/socket/index.js
+++
@@ -1,8 +0,0 @@
-import os from 'node:os'
-
-export async function load () {
-  const platform = os.platform()
-  const arch = os.arch()
-  const info = await import(`@socketsupply/socket-${platform}-${arch}`)
-  return info?.default ?? info ?? null
-}
