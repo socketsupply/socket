e130d6fe6d9efb80572345f405df355564a3aec6
Joseph Werle <joseph.werle@gmail.com>
2023-01-31 15:02:27 +0100

    fix(test/build.js): properly inline runtime imports for win32

--- test/build.js
+++ test/build.js
@@ -1,9 +1,12 @@
 #!/usr/bin/env node
+import { readFileSync } from 'node:fs'
 import path from 'node:path'
 import fs from 'node:fs/promises'
 import os from 'node:os'
 
 import esbuild from 'esbuild'
+
+const dirname = path.dirname(import.meta.url.replace('file://', ''))
 
 const cp = async (a, b) => fs.cp(
   path.resolve(a),
@@ -32,20 +35,21 @@ async function main () {
     keepNames: true,
     platform: 'browser',
     sourcemap: 'inline',
-    external: ['socket:*', 'node:*'],
+    external: ['node:*'],
     outdir: path.resolve(process.argv[2]),
     plugins: []
   }
 
-  if (os.platform() === 'win32') {
+  if (os.platform() !== 'win32') {
+    params.external.push('socket:*')
+  } else {
     params.plugins.push({
       name: 'socket-runtime-import-path',
       setup (build) {
         build.onResolve({ filter: /^socket:.*$/ }, (args) => {
-          return {
-            external: true,
-            path: args.path.replace('socket:', `.${path.sep}socket${path.sep}`).replace(/.js$/, '') + '.js'
-          }
+          const basename = args.path.replace('socket:', '').replace(/.js$/, '') + '.js'
+          const filename = path.resolve(path.dirname(dirname), 'api', filename)
+          return { path: filename }
         })
       }
     })
