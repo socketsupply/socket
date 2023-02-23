d59e8b8cac4c24e30d5f545ed20285333fdecd44
Michael Ribbons <mribbons@gmail.com>
2023-02-24 09:51:07 +1100

    socket-win32-x64: publish, fix install
    Don't call install-pre-reqs during preintall:
    Can't get stdio.input for prompts
    create-socket-app will launch

--- npm/packages/@socketsupply/socket-win32-x64/bin/install-pre-reqs.js
+++ npm/packages/@socketsupply/socket-win32-x64/bin/install-pre-reqs.js
@@ -1,9 +1,22 @@
 #!/usr/bin/env node
 
-import { execSync as exec } from 'node:child_process'
+import { execSync as exec, spawn } from 'node:child_process'
 
-async function main () {
-  exec('cd ps1 && powershell .\\install.ps1 -shbuild:$false', { stdio: 'inherit' })
+async function main() {
+  let pscmd = '"cd ps1 && powershell .\\install.ps1 -shbuild:$false && exit"';
+  let npm_cmd = `%comspec% /k ${pscmd}`;
+  process.stdout.write(npm_cmd);
+
+  const c = spawn(
+    '%comspec%',
+    ['/k', pscmd],
+    { detached: true, shell: true, windowsHide: false }
+  );
+
+  await new Promise((resolve) => {
+    c.on('close', resolve);
+    process.stdout.write("npm completed");
+  });
 }
 
 main().catch((err) => {
