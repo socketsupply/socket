498dffd4a45ec6c767c575b088c682ded5933596
Michael Ribbons <mribbons@gmail.com>
2023-02-26 21:04:15 +1100

    socket-win32-x64-0.0.5: adhere to linting

--- npm/packages/@socketsupply/socket-win32-x64/bin/install-pre-reqs.js
+++ npm/packages/@socketsupply/socket-win32-x64/bin/install-pre-reqs.js
@@ -4,64 +4,60 @@ import { spawn } from 'node:child_process'
 import fs from 'node:fs'
 import path from 'node:path'
 
-async function insert_env_vars() {
+async function insertEnvVars () {
   if (!fs.existsSync('ps1/env.json')) {
-    console.log('WARNING: No env.json, builds will fail - please report (https://discord.gg/YPV32gKCsH)');
-    return;
+    console.log('WARNING: No env.json, builds will fail - please report (https://discord.gg/YPV32gKCsH)')
+    return
   }
 
-  let env_json = fs.readFileSync('ps1/env.json', "utf16le");
-  while (!env_json.startsWith(' ') && !env_json.startsWith('{')) {
-    env_json = env_json.substring(1);
+  let envJSON = fs.readFileSync('ps1/env.json', 'utf16le')
+  while (!envJSON.startsWith(' ') && !envJSON.startsWith('{')) {
+    envJSON = envJSON.substring(1)
   }
 
-  var env = JSON.parse(env_json);
+  const env = JSON.parse(envJSON)
 
-  var envs = '';
+  let envs = ''
   for (const e in env) {
-    envs += `SET ${e}=${env[e]}\r\n`;
+    envs += `SET ${e}=${env[e]}\r\n`
   }
 
-  console.log(envs);
+  console.log(envs)
 
-  fs.writeFileSync("env.bat", envs);
+  fs.writeFileSync('env.bat', envs)
 
-  var ssc_cmd_path = path.join(process.cwd(), '../../.bin/ssc.cmd');
+  const sscCmdPath = path.join(process.cwd(), '../../.bin/ssc.cmd')
 
-
-  if (!fs.existsSync(ssc_cmd_path)) {
-    console.log(`WARNING: No ${ssc_cmd_path} builds will fail - please report (https://discord.gg/YPV32gKCsH)`);
-    return;
+  if (!fs.existsSync(sscCmdPath)) {
+    console.log(`WARNING: No ${sscCmdPath} builds will fail - please report (https://discord.gg/YPV32gKCsH)`)
+    return
   }
 
-  fs.writeFileSync(ssc_cmd_path, envs + fs.readFileSync(ssc_cmd_path), "utf-8");
+  fs.writeFileSync(sscCmdPath, envs + fs.readFileSync(sscCmdPath), 'utf-8')
 }
 
-async function preinstall() {
-  let pscmd = '"cd ps1 && powershell .\\install.ps1 -shbuild:$false -package_setup:$true "';
-  let npm_cmd = `%comspec% /k ${pscmd}`;
-  process.stdout.write(npm_cmd + '\n');
+async function preinstall () {
+  const psCommand = '"cd ps1 && powershell .\\install.ps1 -shbuild:$false -package_setup:$true "'
+  const npmCommand = `%comspec% /k ${psCommand}`
+  process.stdout.write(npmCommand + '\n')
 
-  const c = spawn(
+  const cmdProcess = spawn(
     '%comspec%',
-    ['/k', pscmd],
+    ['/k', psCommand],
     { detached: true, shell: true, windowsHide: false }
-  );
+  )
 
-  await new Promise((resolve) => {
-    c.on('close', resolve);
-    process.stdout.write("npm completed");
-  });
+  await new Promise((resolve, reject) => {
+    cmdProcess.on('close', resolve).on('error', reject)
+    process.stdout.write('npm completed')
+  })
 }
 
-async function main(argv) {
-  if (argv.length == 0)
-    return;
+async function main (argv) {
+  if (argv.length === 0) { return }
 
-  if (argv[0] == 'preinstall')
-    await preinstall();
-  if (argv[0] == 'postinstall')
-    await insert_env_vars();
+  if (argv[0] === 'preinstall') { await preinstall() }
+  if (argv[0] === 'postinstall') { await insertEnvVars() }
 }
 
 main(process.argv.slice(2)).catch((err) => {
