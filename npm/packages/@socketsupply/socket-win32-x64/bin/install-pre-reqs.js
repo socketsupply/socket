681822a32aa08d57c6fdde8df64068aaddb6711d
Mike Ribbons <mribbons@gmail.com>
2023-03-03 10:33:44 +1100

    Socket win32 x64 0.0.4 (#202)
    * socket-win32-x64: publish
    
    * socket-win32-x64: publish, fix install
    
    Don't call install-pre-reqs during preintall:
    Can't get stdio.input for prompts
    create-socket-app will launch
    
    * socket-win32-x64: @socketsupply/socket version++
    
    * socket-win32-x64-0.0.4: supply CXX to ssc.cmd
    
    This was the best way I could see to get clang's path through to ssc
    Nothing else worked, npm bins etc.

--- npm/packages/@socketsupply/socket-win32-x64/bin/install-pre-reqs.js
+++ npm/packages/@socketsupply/socket-win32-x64/bin/install-pre-reqs.js
@@ -1,11 +1,46 @@
 #!/usr/bin/env node
 
-import { execSync as exec, spawn } from 'node:child_process'
+import { spawn } from 'node:child_process'
+import fs from 'node:fs'
+import path from 'node:path'
+
+async function insert_env_vars() {
+  if (!fs.existsSync('ps1/env.json')) {
+    console.log('WARNING: No env.json, builds will fail - please report (https://discord.gg/YPV32gKCsH)');
+    return;
+  }
+
+  let env_json = fs.readFileSync('ps1/env.json', "utf16le");
+  while (!env_json.startsWith(' ') && !env_json.startsWith('{')) {
+    env_json = env_json.substring(1);
+  }
+
+  var env = JSON.parse(env_json);
+
+  var envs = '';
+  for (const e in env) {
+    envs += `SET ${e}=${env[e]}\r\n`;
+  }
+
+  console.log(envs);
+
+  fs.writeFileSync("env.bat", envs);
+
+  var ssc_cmd_path = path.join(process.cwd(), '../../.bin/ssc.cmd');
+
+
+  if (!fs.existsSync(ssc_cmd_path)) {
+    console.log(`WARNING: No ${ssc_cmd_path} builds will fail - please report (https://discord.gg/YPV32gKCsH)`);
+    return;
+  }
+
+  fs.writeFileSync(ssc_cmd_path, envs + fs.readFileSync(ssc_cmd_path), "utf-8");
+}
 
 async function main() {
-  let pscmd = '"cd ps1 && powershell .\\install.ps1 -shbuild:$false && exit"';
+  let pscmd = '"cd ps1 && powershell .\\install.ps1 -shbuild:$false -package_setup:$true "';
   let npm_cmd = `%comspec% /k ${pscmd}`;
-  process.stdout.write(npm_cmd);
+  process.stdout.write(npm_cmd + '\n');
 
   const c = spawn(
     '%comspec%',
@@ -17,6 +52,8 @@ async function main() {
     c.on('close', resolve);
     process.stdout.write("npm completed");
   });
+
+  await insert_env_vars();
 }
 
 main().catch((err) => {
