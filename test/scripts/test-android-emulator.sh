504916654eb8950d838dbc81110ca6e88f054d5a
Joseph Werle <joseph.werle@gmail.com>
2023-03-08 13:16:26 -0500

    fix(test/scripts/test-android-emulator.sh): determine 'adb' in env

--- test/scripts/test-android-emulator.sh
+++ test/scripts/test-android-emulator.sh
@@ -1,34 +1,35 @@
 #!/usr/bin/env bash
 
 id="co.socketsupply.socket.tests"
+adb="$ANDROID_HOME/platform-tools/adb"
 root="$(CDPATH='' cd -- "$(dirname "$(dirname -- "$0")")" && pwd)"
 
 ${SHELL:-sh} "$root/scripts/bootstrap-android-emulator.sh" &
 
 echo "info: Waiting for Android Emulator to boot"
-while ! adb shell getprop sys.boot_completed >/dev/null 2>&1 ; do
+while ! "$adb" shell getprop sys.boot_completed >/dev/null 2>&1 ; do
   sleep 0.5
 done
 echo "info: Android Emulator booted"
 
-adb uninstall "$id"
+"$adb" uninstall "$id"
 ssc build --headless --platform=android -r -o || {
   rc=$?
   echo "info: Shutting Android Emulator"
-  adb devices | grep emulator | cut -f1 | while read -r line; do
-    adb -s "$line" emu kill
+  "$adb" devices | grep emulator | cut -f1 | while read -r line; do
+    "$adb" -s "$line" emu kill
   done
   exit "$rc"
 }
 
-adb shell rm -rf "/data/local/tmp/ssc-socket-test-fixtures"
-adb push "$root/fixtures/" "/data/local/tmp/ssc-socket-test-fixtures"
+"$adb" shell rm -rf "/data/local/tmp/ssc-socket-test-fixtures"
+"$adb" push "$root/fixtures/" "/data/local/tmp/ssc-socket-test-fixtures"
 
 ${SHELL:-sh} "$root/scripts/poll-adb-logcat.sh"
 
 echo "info: Shutting Android Emulator"
-adb devices | grep emulator | cut -f1 | while read -r line; do
-  adb -s "$line" emu kill
+"$adb" devices | grep emulator | cut -f1 | while read -r line; do
+  "$adb" -s "$line" emu kill
 done
 
-adb kill-server
+"$adb" kill-server
