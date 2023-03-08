4e5ac3c21a530bd9abc922022579ea51e2d07cf5
Joseph Werle <joseph.werle@gmail.com>
2023-03-08 13:15:48 -0500

    refactor(api/internal/init.js): make preload runtime in worker env

--- api/internal/init.js
+++ api/internal/init.js
@@ -1,4 +1,4 @@
-/* global CustomEvent */
+/* global CustomEvent, Blob */
 /* eslint-disable import/first */
 globalThis.__RUNTIME_INIT_NOW__ = performance.now()
 
@@ -8,17 +8,8 @@ import globals from './globals.js'
 import hooks from '../hooks.js'
 import ipc from '../ipc.js'
 
-// async preload modules
-hooks.onReady(async () => {
-  // precache fs.constants
-  await ipc.request('fs.constants', {}, { cache: true })
-  import('../diagnostics.js')
-  import('../fs/fds.js')
-  import('../fs/constants.js')
-})
-
 class RuntimeXHRPostQueue extends EventTarget {
-  concurrency = 16
+  concurrency = 4 * (globalThis?.navigator?.hardwareConcurrency || 4)
   pending = []
 
   async wait () {
@@ -60,6 +51,72 @@ class RuntimeXHRPostQueue extends EventTarget {
       globalThis.dispatchEvent(new CustomEvent('data', { detail }))
       promise.resolve()
     })
+  }
+}
+
+if (typeof globalThis?.Worker === 'function' && globalThis.window) {
+  // async preload modules
+  hooks.onReady(async () => {
+    // precache fs.constants
+    await ipc.request('fs.constants', {}, { cache: true })
+    import('../diagnostics.js')
+    import('../fs/fds.js')
+    import('../fs/constants.js')
+  })
+
+  globalThis.Worker = class Worker extends globalThis?.Worker {
+    #onmessage = null
+
+    constructor (url, options, ...args) {
+      const preload = `
+      globalThis.__args = ${JSON.stringify(globalThis.__args)}
+      await import('socket:internal/init')
+      await import('socket:internal/worker')
+      await import('${new URL(url, globalThis.location?.href || '/')}')
+      `
+
+      const blob = new Blob([preload], { type: 'application/javascript' })
+      const uri = URL.createObjectURL(blob)
+
+      super(uri, { ...options, type: 'module' }, ...args)
+
+      this.addEventListener('message', (event) => {
+        const { data } = event
+        if (data?.__runtime_worker_ipc_request) {
+          const request = data.__runtime_worker_ipc_request
+          if (
+            typeof request?.message === 'string' &&
+            request.message.startsWith('ipc://')
+          ) {
+            queueMicrotask(async () => {
+              try {
+                const message = ipc.Message.from(request.message, request.bytes)
+                const options = { bytes: message.bytes }
+                const result = await ipc.send(message.name, message.rawParams, options)
+                this.postMessage({
+                  __runtime_worker_ipc_result: JSON.parse(JSON.stringify({
+                    message,
+                    result
+                  }))
+                })
+              } catch (err) {
+                console.warn('__runtime_worker_ipc_result:', err.stack || err)
+              }
+            })
+          }
+        } else if (typeof this.onmessage === 'function') {
+          return this.onmessage(event)
+        }
+      })
+    }
+
+    get onmessage () {
+      return this.#onmessage
+    }
+
+    set onmessage (onmessage) {
+      this.#onmessage = onmessage
+    }
   }
 }
 
