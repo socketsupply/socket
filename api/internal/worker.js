612de40431490f8d33c6c7a4b663f79954bc7d7b
Joseph Werle <joseph.werle@gmail.com>
2023-03-10 13:29:50 -0500

    refactor(api/internal/worker.js): improve worker context, handle events

--- api/internal/worker.js
+++ api/internal/worker.js
@@ -1,26 +1,77 @@
 /* global CustomEvent */
+import './init.js'
 import ipc from '../ipc.js'
+import hooks from '../hooks.js'
+import console from '../console.js'
 
-if (typeof globalThis?.addEventListener === 'function') {
-  globalThis.addEventListener('message', onMessage)
+export default undefined // we MUST export _something_
+
+let didImportSource = false
+
+if (typeof globalThis.addEventListener === 'function') {
+  globalThis.addEventListener('message', onWorkerMessage)
 }
 
-export function onMessage (event) {
+hooks.onReady(() => {
+  globalThis.postMessage({ __runtime_worker_request_args: true })
+})
+
+let onmessage = null
+Object.defineProperty(globalThis, 'onmessage', {
+  get: () => onmessage,
+  set: (value) => {
+    onmessage = value
+  }
+})
+
+export function onWorkerMessage (event) {
   const { data } = event
 
-  if (typeof data?.__runtime_worker_ipc_result === 'object') {
-    const resultData = data?.__runtime_worker_ipc_result || {}
-    const message = ipc.Message.from(resultData.message)
-    const { seq } = message
-    const index = globalThis.__args.index
-    const event = new CustomEvent(`resolve-${index}-${seq}`, {
-      detail: resultData.result
-    })
+  if (typeof data?.__runtime_worker_args === 'object') {
+    globalThis.__args = data.__runtime_worker_args
 
-    globalThis.dispatchEvent(event)
+    if (!didImportSource) {
+      didImportSource = true
+      hooks.onReady(async () => {
+        const url = new URL(import.meta.url)
+        const source = url.searchParams.get('source')
+
+        try {
+          await import(source)
+        } catch (err) {
+          console.error(
+            'RuntimeWorker: Failed to import worker:', err.stack || err
+          )
+        }
+      })
+    }
+
+    event.stopImmediatePropagation()
+    return false
+  } else if (typeof data?.__runtime_worker_ipc_result === 'object') {
+    const resultData = data?.__runtime_worker_ipc_result || {}
+    if (resultData.message && resultData.result) {
+      const message = ipc.Message.from(resultData.message)
+      const { seq } = message
+      const index = globalThis.__args.index
+
+      globalThis.dispatchEvent(new CustomEvent(`resolve-${index}-${seq}`, {
+        detail: resultData.result
+      }))
+    }
+
+    event.stopImmediatePropagation()
+    return false
+  } else if (typeof data?.__runtime_worker_event === 'object') {
+    const { type, detail } = data?.__runtime_worker_event || {}
+    if (type) {
+      globalThis.dispatchEvent(new CustomEvent(type, { detail }))
+    }
     event.stopImmediatePropagation()
     return false
   }
-}
 
-export * as default from './worker.js'
+  if (typeof onmessage === 'function') {
+    return onmessage(event)
+  }
+}
