39af2209a7f894704c39cd952bd653ad74b1a49d
Joseph Werle <joseph.werle@gmail.com>
2023-03-11 19:35:54 -0500

    feat(api/path): initial 'socket:path' implementation

--- api/path/win32.js
+++ api/path/win32.js
@@ -1,6 +1,98 @@
 import { Path } from './path.js'
+import posix from './posix.js'
 
-export default class Win32Path extends Path {
-  static get sep () { return '\\' }
-  static get delimiter () { return ';' }
+export { posix }
+export * as default from './win32.js'
+export * as win32 from './win32.js'
+
+export const sep = '\\'
+export const delimiter = ';'
+
+/**
+ * Computes current working directory for a path
+ * @param {string}
+ */
+export function cwd () {
+  return Path.cwd()
+}
+
+/**
+ * Resolves path components to an absolute path.
+ * @param {...PathComponent} components
+ * @return {string}
+ */
+export function resolve (...components) {
+  return Path.resolve({ sep }, ...components)
+}
+
+/**
+ * Joins path components. This function may not return an absolute path.
+ * @param {...PathComponent} components
+ * @return {string}
+ */
+export function join (...components) {
+  return Path.join({ sep }, ...components)
+}
+
+/**
+ * Computes directory name of path.
+ * @param {...PathComponent} components
+ * @return {string}
+ */
+export function dirname (path) {
+  return Path.dirname({ sep }, path)
+}
+
+/**
+ * Computes base name of path.
+ * @param {...PathComponent} components
+ * @return {string}
+ */
+export function basename (path) {
+  return Path.basename({ sep }, path)
+}
+
+/**
+ * Computes extension name of path.
+ * @param {PathComponent} path
+ * @return {string}
+ */
+export function extname (path) {
+  return Path.extname({ sep }, path)
+}
+
+/**
+ * Predicate helper to determine if path is absolute.
+ * @param {string} path
+ * @return {boolean}
+ */
+export function isAbsolute (path) {
+  return Path.from(path, '.').isRelative === false
+}
+
+/**
+ * Parses input `path` into a `Path` instance.
+ * @param {PathComponent} path
+ * @return {Path}
+ */
+export function parse (path) {
+  return Path.from(path)
+}
+
+/**
+ * Formats `Path` object into a string.
+ * @param {object|Path} path
+ * @return {string}
+ */
+export function format (path) {
+  return Path.format({ sep: path.sep || sep }, path)
+}
+
+/**
+ * Normalizes `path` resolving `..` and `.\` preserving trailing
+ * slashes.
+ * @param {string} path
+ */
+export function normalize (path) {
+  return Path.normalize({ sep }, path)
 }
