55ebbe1262e322a2670b18987749fa9600610545
Joseph Werle <joseph.werle@gmail.com>
2023-03-15 15:52:35 -0400

    chore(): prepare for stream-relay sources with script

--- api/p2p.js
+++ api/p2p.js
@@ -1,15 +1,16 @@
 /**
  * @module P2P
  *
- * A low-level P2P module for networking that allows you to discover peers,
- * connect to peers, and send packets reliably.
+ * A low-level network protocol for P2P.
  *
  * @see {@link https://github.com/socketsupply/stream-relay}
  *
  */
-// import { createPeer } from './external/stream-relay/src/index.js'
-// import dgram from './dgram.js'
+import { createPeer } from './stream-relay/index.js'
 
-// export * from './external/stream-relay/src/index.js'
-// const Peer = createPeer(dgram)
-// export { Peer }
+import dgram from './dgram.js'
+
+const Peer = createPeer(dgram)
+
+export * from './stream-relay/index.js'
+export { Peer }
