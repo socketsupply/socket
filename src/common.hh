69eaad3a6e1e99308577298c04840366cfa6abc3
Joseph Werle <joseph.werle@gmail.com>
2023-03-20 13:42:53 -0400

    refactor(src/cli): reuse 'parseConfig()' for build copy map, support build variants

--- src/common.hh
+++ src/common.hh
@@ -636,7 +636,10 @@ namespace SSC {
     for (auto entry : entries) {
       entry = trim(entry);
 
-      if (entry[0] == ';') continue;
+      // handle a variety of comment styles
+      if (entry[0] == ';' || entry[0] == '#') {
+        continue;
+      }
 
       if (entry[0] == '[' && entry[entry.length() - 1] == ']') {
         prefix = entry.substr(1, entry.length() - 2);
