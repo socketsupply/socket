1167f4aacf14a4a02e184f4d647a9e69f04008b2
Mike Ribbons <mribbons@gmail.com>
2023-04-11 08:55:02 +1000

    install.ps1: fix issue when cmake installed separately (#322)
    * install.ps1: fix issue when cmake installed separately
    
    Resolves https://github.com/socketsupply/socket/issues/321
    
    * install.ps1: fix cmake path detection

--- bin/install.ps1
+++ bin/install.ps1
@@ -975,7 +975,7 @@ Download size: 5.5GB, Installed size: 10.2GB y/[N]"
     
   Exit-IfErrors
 
-  if ($confirm_windows_deps) {
+  if ($confirm_windows_deps -or $shbuild) {
     if ($install_vc_build) {
       $vc_exists, $vc_vars = $(Get-VCVars)
       if ($vc_exists) {
@@ -1021,12 +1021,22 @@ Download size: 5.5GB, Installed size: 10.2GB y/[N]"
     }
 
     if ($shbuild) {
+      $cmake_found = $false
       if (-not (Test-CommandVersion("cmake", $targetCmakeVersion))) {
-        $global:install_errors += "not ok - unable to install cmake"
-      } else {
-        if ($cmakePath -ne '') {
+        $cmakePath = "$env:ProgramFiles\CMake\bin"
+        if ((Test-Path "$cmakePath\cmake.exe" -PathType Leaf) -eq $true) {
           $env:PATH="$cmakePath\;$env:PATH"
+          $global:cmake = "$cmakePath\$global:cmake"
+          if (Test-CommandVersion("cmake", $targetCmakeVersion)) {
+            $cmake_found = $true
+          }
         }
+      } else {        
+        $cmake_found = $true
+      }
+
+      if ($cmake_found -eq $false) {
+        $global:install_errors += "not ok - unable to install cmake"
       }
     }
 
