9beeeb6f2d5f441d1d88c8ee1570ec5aeb644357
Mike Ribbons <mribbons@gmail.com>
2023-04-16 17:31:53 +1000

    windows fte: Locate vcvars64.bat more reliably
    Resolves https://github.com/socketsupply/socket/issues/316

--- bin/Get-VCVars.ps1
+++ bin/Get-VCVars.ps1
@@ -1,13 +1,30 @@
 Function Get-VCVars {
+  $vs_releases = @('2022')
+
+  # Locate VC Vars outside of %ProgramFiles(x86)%
+  $shortcut_path_template="$env:ProgramData\Microsoft\Windows\Start Menu\Programs\Visual Studio %RELEASE\Visual Studio Tools\VC\x64 Native Tools Command Prompt for VS %RELEASE.lnk"
+
+  foreach ($r in $vs_releases) {
+    $shortcut_path = $shortcut_path_template.replace("%RELEASE", $r)
+    if (Test-Path $shortcut_path -PathType leaf) {
+      $WScript = New-Object -ComObject WScript.Shell
+      $shortcut = $WScript.CreateShortcut($shortcut_path)
+      $test_path = $shortcut.Arguments.replace("/k ", "").replace("""", "")
+      if (Test-Path $test_path -PathType leaf) {
+        return $true, $test_path
+      }
+    }
+  }
+
+  # Fall back to previous method
   $pf = [Environment]::GetEnvironmentVariable("ProgramFiles(x86)")
   $mvs = "Microsoft Visual Studio"
   $suffix = "VC\Auxiliary\Build\vcvars64.bat"
 
-  $vs_releases = @('2022')
   $vs_editions = @('Enterprise', 'Professional', 'Community', 'BuildTools')
 
-  Foreach ($r in $vs_releases) {
-    Foreach ($e in $vs_editions) {
+  foreach ($r in $vs_releases) {
+    foreach ($e in $vs_editions) {
       $test_path = "$pf\$mvs\$r\$e\$suffix"
       if (Test-Path $test_path -PathType leaf) {
         return $true, $test_path
