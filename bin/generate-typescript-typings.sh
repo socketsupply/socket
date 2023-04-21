d13ed1ad17f74ad463daa5b666ec0bdf912fb87b
Joseph Werle <joseph.werle@gmail.com>
2023-04-21 09:35:35 -0700

    refactor(): clean up

--- bin/generate-typescript-typings.sh
+++ bin/generate-typescript-typings.sh
@@ -4,7 +4,7 @@ declare root="$(cd "$(dirname "$(dirname "${BASH_SOURCE[0]}")")" && pwd)"
 
 cd "$root" || exit $?
 
-tsc || exit $?
+tsc --emitDeclarationOnly --module es2022 --outFile api/index || exit $?
 
 sed -i '' -e 's/declare module "\(.*\)"/declare module "socket:\1"/g' api/index.d.ts || exit $?
 sed -i '' -e 's/namespace \_\_\_.*$//g' api/index.d.ts || exit $?
