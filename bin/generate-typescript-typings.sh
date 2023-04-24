1be3fcfd3f13e6f93c20e5e166004cb88eaeeaac
Sergey Rubanov <chi187@gmail.com>
2023-04-24 21:31:12 +0400

    chore(CI): cleanup

--- bin/generate-typescript-typings.sh
+++ bin/generate-typescript-typings.sh
@@ -6,5 +6,7 @@ cd "$root" || exit $?
 
 tsc --emitDeclarationOnly --module es2022 --outFile api/index || exit $?
 
-sed -i '' -e 's/declare module "\(.*\)"/declare module "socket:\1"/g' api/index.d.ts || exit $?
-sed -i '' -e 's/namespace \_\_\_.*$//g' api/index.d.ts || exit $?
+ls -la api
+
+sed -i -e 's/declare module "\(.*\)"/declare module "socket:\1"/g' api/index.d.ts || exit $?
+sed -i -e 's/namespace \_\_\_.*$//g' api/index.d.ts || exit $?
