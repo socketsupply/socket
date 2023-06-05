feb35992f450c63fd004d33f49d8ad2c40b3ac83
Joseph Werle <joseph.werle@gmail.com>
2023-06-05 12:11:18 +0200

    fix(api/{diagnostics,dns,fs}.js): fix re-exports

--- api/dns.js
+++ api/dns.js
@@ -3,7 +3,6 @@
  * @notice This is a rexports of `dns/index.js` so consumers will
  * need to only `import dns from 'socket:dns'`
  */
-import * as exports from './dns.js'
-
+import * as exports from './dns/index.js'
 export * from './dns/index.js'
 export default exports
