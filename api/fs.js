0f37c5fa65ce7dc094e526202fc33d92eaf1814b
Joseph Werle <joseph.werle@gmail.com>
2023-06-05 12:11:18 +0200

    fix(api/{diagnostics,dns,fs}.js): fix re-exports

--- api/fs.js
+++ api/fs.js
@@ -2,7 +2,6 @@
  * @notice This is a rexports of `fs/index.js` so consumers will
  * need to only `import * as fs from 'socket:fs'`
  */
-import * as exports from './fs.js'
-
+import * as exports from './fs/index.js'
 export * from './fs/index.js'
 export default exports
