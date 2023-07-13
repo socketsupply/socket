a9d4371c912a0de7142b4d485af37e00e845013e
Joseph Werle <joseph.werle@gmail.com>
2023-07-13 16:46:09 +0200

    refactor(src/cli): support '--root' in 'print-build-dir' command

--- src/cli/templates.hh
+++ src/cli/templates.hh
@@ -103,8 +103,8 @@ usage:
   ssc print-build-dir [--platform=<platform>] [--prod] [<project-dir>]
 
 options:
-  --platform  ios; if not specified, runs on the current platform
-  --prod      use a production build directory
+  --platform  android|android-emulator|ios|ios-simulator; if not specified, runs on the current platform
+  --root      print root build directory
 )TEXT";
 
 constexpr auto gHelpTextRun = R"TEXT(
@@ -114,7 +114,7 @@ usage:
   ssc run [options] [<project-dir>]
 
 options:
-  --platform     ios-simulator; if not specified, runs on the current platform
+  --platform     android|ios|ios-simulator; if not specified, runs on the current platform
   --prod         run production build
   --test=path    indicate test mode
 )TEXT";
