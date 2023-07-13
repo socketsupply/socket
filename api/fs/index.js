1bcc4f1c8934e343cbb9e1c2ba4cfc67869a4855
Peter Johnson <insomnia@rcpt.at>
2023-07-13 17:28:45 +0200

    fix(fs): variable name typo (#501)

--- api/fs/index.js
+++ api/fs/index.js
@@ -309,7 +309,7 @@ export function mkdir (path, options, callback) {
   }
 
   const mode = options.mode || 0o777
-  const recursive = options.recurisve === true
+  const recursive = options.recursive === true
 
   if (typeof mode !== 'number') {
     throw new TypeError('mode must be a number.')
