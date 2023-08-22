9c595d263fdc458b4d72a39f2a158707ea78f499
Joseph Werle <joseph.werle@gmail.com>
2023-08-22 16:55:31 +0200

    refactor(src/core): clean up and improve 'Headers' implementation

--- src/core/core.cc
+++ src/core/core.cc
@@ -7,8 +7,17 @@ namespace SSC {
   }
 
   Headers::Header::Header (const String& key, const Value& value) {
-    this->key = key;
-    this->value = value;
+    this->key = trim(key);
+    this->value = trim(value.str());
+  }
+
+  Headers::Headers (const String& source) {
+    for (const auto& entry : split(source, '\n')) {
+      const auto tuple = split(entry, ':');
+      if (tuple.size() == 2) {
+        set(trim(tuple.front()), trim(tuple.back()));
+      }
+    }
   }
 
   Headers::Headers (const Headers& headers) {
@@ -29,6 +38,43 @@ namespace SSC {
     }
   }
 
+  void Headers::set (const String& key, const String& value) {
+    set(Header{ key, value });
+  }
+
+  void Headers::set (const Header& header) {
+    for (auto& entry : entries) {
+      if (header.key == entry.key) {
+        entry.value = header.value;
+        return;
+      }
+    }
+
+    entries.push_back(header);
+  }
+
+  bool Headers::has (const String& name) const {
+    for (const auto& header : entries) {
+      if (header.key == name) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  const Headers::Header& Headers::get (const String& name) const {
+    static const auto empty = Header();
+
+    for (const auto& header : entries) {
+      if (header.key == name) {
+        return header;
+      }
+    }
+
+    return empty;
+  }
+
   size_t Headers::size () const {
     return this->entries.size();
   }
@@ -43,6 +89,56 @@ namespace SSC {
       }
     }
     return headers.str();
+  }
+
+  Headers::Value::Value (const String& value) {
+    this->string = trim(value);
+  }
+
+  Headers::Value::Value (const char* value) {
+    this->string = value;
+  }
+
+  Headers::Value::Value (const Value& value) {
+    this->string = value.string;
+  }
+
+  Headers::Value::Value (bool value) {
+    this->string = value ? "true" : "false";
+  }
+
+  Headers::Value::Value (int value) {
+    this->string = std::to_string(value);
+  }
+
+  Headers::Value::Value (float value) {
+    this->string = std::to_string(value);
+  }
+
+  Headers::Value::Value (int64_t value) {
+    this->string = std::to_string(value);
+  }
+
+  Headers::Value::Value (uint64_t value) {
+    this->string = std::to_string(value);
+  }
+
+  Headers::Value::Value (double_t value) {
+    this->string = std::to_string(value);
+  }
+
+#if defined(__APPLE__)
+  Headers::Value::Value (ssize_t value) {
+    this->string = std::to_string(value);
+  }
+#endif
+
+  const String& Headers::Value::str () const {
+    return this->string;
+  }
+
+  const char * Headers::Value::c_str() const {
+    return this->str().c_str();
   }
 
   Post Core::getPost (uint64_t id) {
