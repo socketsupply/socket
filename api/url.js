77f03b4dd61a0a53ffa38ddfc9c03e8318b8960a
Joseph Werle <joseph.werle@gmail.com>
2023-08-22 17:16:20 +0200

    feat(api/url.js): top level 'url' module

---
+++ api/url.js
@@ -0,0 +1,7 @@
+/**
+ * @notice This is a rexports of `url/index.js` so consumers will
+ * need to only `import * as url from 'socket:url'`
+ */
+import * as exports from './url/index.js'
+export * from './url/index.js'
+export default exports
