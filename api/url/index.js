29805c729f2f6da210727ea7f9b38162728135cf
Joseph Werle <joseph.werle@gmail.com>
2023-08-27 19:47:49 -0400

    refactor(api/url/index.js): handle 'socket' and 'ipc' based URLs

--- api/url/index.js
+++ api/url/index.js
@@ -1,7 +1,13 @@
 import { URLPattern } from './urlpattern/urlpattern.js'
 import url from './url/url.js'
 
-const { URL, URLSearchParams, parseURL } = url
+const {
+  URL,
+  URLSearchParams,
+  parseURL,
+  serializeURLOrigin,
+  serializeHost
+} = url
 
 export default URL
 export { URLPattern, URL, URLSearchParams, parseURL }
@@ -10,10 +16,22 @@ export const parse = parseURL
 
 // lifted from node
 export function resolve (from, to) {
-  const resolvedUrl = new URL(to, new URL(from, 'resolve://'));
-  if (resolvedUrl.protocol === 'resolve:') {
-    const { pathname, search, hash } = resolvedUrl;
-    return pathname + search + hash;
+  const resolved = new URL(to, new URL(from, 'resolve://'))
+
+  if (resolved.protocol === 'resolve:') {
+    const { pathname, search, hash } = resolved
+    return pathname + search + hash
   }
-  return resolvedUrl.toString();
+
+  return resolved.toString()
+}
+
+url.serializeURLOrigin = function (input) {
+  const { scheme, host } = input
+
+  if (scheme === 'socket' || scheme === 'ipc') {
+    return `${scheme}://${serializeHost(host)}`
+  }
+
+  return serializeURLOrigin(input)
 }
