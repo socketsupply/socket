eddfcca862958cf8d22921820fdbf32fac1ca59f
Joseph Werle <joseph.werle@gmail.com>
2023-08-27 19:53:56 -0400

    refactor(api/fetch): ignore 'socket:fetch'

--- api/fetch.js
+++ api/fetch.js
@@ -1,4 +1,5 @@
 /**
+ * @ignore
  * @notice This is a rexports of `fetch/index.js` so consumers will
  * need to only `import * as fetch from 'socket:fetch'`
  */
--- api/fetch/index.js
+++ api/fetch/index.js
@@ -1,3 +1,6 @@
+/**
+ * @ignore
+ */
 import { fetch } from './fetch.js'
 export * from './fetch.js'
 export default fetch
