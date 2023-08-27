85ff5877240c37de6f33dc0463c442aba0fd9a9b
Joseph Werle <joseph.werle@gmail.com>
2023-08-27 19:29:43 -0400

    feat(api/fetch): use 'whatwg-fetch'

---
+++ api/fetch.js
@@ -0,0 +1,7 @@
+/**
+ * @notice This is a rexports of `fetch/index.js` so consumers will
+ * need to only `import * as fetch from 'socket:fetch'`
+ */
+import fetch from './fetch/index.js'
+export * from './fetch/index.js'
+export default fetch
---
+++ api/fetch/index.js
@@ -0,0 +1,3 @@
+import { fetch } from './fetch.js'
+export * from './fetch.js'
+export default fetch
