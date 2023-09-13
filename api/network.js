7ceb775dc53b511d6849eda0291e279cec17356e
heapwolf <paolo@socketsupply.co>
2023-09-13 12:56:13 +0200

    replaces peer with network

---
+++ api/network.js
@@ -0,0 +1,19 @@
+/*
+ *
+ *
+ * @module Network
+ *
+ * Provides a higher level API over the stream-relay protocol.
+ *
+ * @see {@link https://socketsupply.co/guides/#p2p-guide}
+ *
+ */
+import sugar from './stream-relay/sugar.js'
+import { Cache, sha256, Encryption, NAT } from './stream-relay/index.js'
+import events from './events.js'
+import dgram from './dgram.js'
+
+const network = sugar(dgram, events)
+
+export { network, Cache, sha256, Encryption, NAT }
+export default network
