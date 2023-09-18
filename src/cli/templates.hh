a47819891fb86e3d76c171e39115b0735feacb9c
Sergey Rubanov <chi187@gmail.com>
2023-09-18 16:01:09 +0200

    feat(src/cli/cli.cc): check if Bundle ID is in reverse DNS notation

--- src/cli/templates.hh
+++ src/cli/templates.hh
@@ -1429,7 +1429,9 @@ flags = "-g"
 
 [meta]
 
-; A unique ID that identifies the bundle (used by all app stores). It's required when `[meta] type` is not `"extension"`.
+; A unique ID that identifies the bundle (used by all app stores).
+; It's required when `[meta] type` is not `"extension"`.
+; It should be in a reverse DNS notation https://developer.apple.com/documentation/bundleresources/information_property_list/cfbundleidentifier#discussion
 bundle_identifier = "com.beepboop"
 
 ; A string that gets used in the about dialog and package meta info.
