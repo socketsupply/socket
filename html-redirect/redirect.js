507a61e44cc90afa9c4e608aaa9d402c9305f1a8
Bret Comnes <bcomnes@gmail.com>
2023-10-06 16:43:28 -0700

    Passing tests

--- html-redirect/redirect.js
+++ html-redirect/redirect.js
@@ -1,4 +1,4 @@
-import { access } from 'fs/promises'
+import { access, stat } from 'fs/promises'
 import path from 'path'
 
 export async function redirect (inputPath, basePath) {
@@ -8,26 +8,22 @@ export async function redirect (inputPath, basePath) {
   // Resolve the full path for easier handling
   const fullPath = path.join(basePath, inputPath)
 
-  // 1. Try the given path
-  console.log(1)
-  if (await exists(fullPath)) {
+  // 1. Try the given path if it's a file
+  if (await isFile(fullPath)) {
     return stripBasePath(fullPath, basePath)
   }
 
   // 2. Try appending a `/` to the path and checking for an index.html
-  console.log(2)
   const indexPath = path.join(fullPath, 'index.html')
   if (await exists(indexPath)) return stripBasePath(indexPath, basePath)
 
   // 3. Check if the input ends with .html, strip it and check
-  console.log(3)
   if (path.extname(fullPath) === '.html') {
     const strippedPath = fullPath.substring(0, fullPath.length - 5)
     if (await exists(strippedPath)) return stripBasePath(strippedPath, basePath)
   }
 
   // 4. Check if a .html file exists for the given path
-  console.log(4)
   const htmlPath = `${fullPath}.html`
   if (await exists(htmlPath)) return stripBasePath(htmlPath, basePath)
 
@@ -35,11 +31,21 @@ export async function redirect (inputPath, basePath) {
   return null
 }
 
-// Helper function to check if a file or directory exists
+// Helper function to check if a path exists
 async function exists (filePath) {
   try {
     await access(filePath)
     return true
+  } catch {
+    return false
+  }
+}
+
+// Helper function to check if a path points to a file
+async function isFile (filePath) {
+  try {
+    const fileStat = await stat(filePath)
+    return fileStat.isFile()
   } catch {
     return false
   }
