dcffb87b80fa3f8c05c3010356748f9c0fc2bdc4
Bret Comnes <bcomnes@gmail.com>
2023-10-06 16:42:08 -0700

    Almost working

--- html-redirect/test-cases/1/test.js
+++ html-redirect/test-cases/1/test.js
@@ -1,12 +1,16 @@
 import test from 'node:test'
 import assert from 'node:assert'
 import desm from '../../desm.js'
-import {join} from 'path'
 import { redirect } from '../../redirect.js'
 
 const __dirname = desm(import.meta.url)
 
 test('index export', async (t) => {
-  const results2 = await redirect(join(__dirname, 'does-not-exist.html'))
-  const results = await redirect(join(__dirname, 'index.html'))
+  // assert.strictEqual(await redirect('/', __dirname), '/index.html')
+  // assert.strictEqual(await redirect('/index.html', __dirname), '/index.html')
+  assert.strictEqual(await redirect('/a-conflict-index', __dirname), '/a-conflict-index/index.html')
+  // assert.strictEqual(await redirect('/another-file', __dirname), '/another-file.html')
+  // assert.strictEqual(await redirect('/an-index-file/', __dirname), '/an-index-file/index.html')
+  // assert.strictEqual(await redirect('/an-index-file', __dirname), '/an-index-file/index.html')
+  // assert.strictEqual(await redirect('/an-index-file/a-html-file', __dirname), '/an-index-file/.html')
 })
