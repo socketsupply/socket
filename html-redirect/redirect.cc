2a3bb5c58e769d266b46f4ea72b9e620e01bdec0
Bret Comnes <bcomnes@gmail.com>
2023-10-06 17:10:29 -0700

    Bugfixes

--- html-redirect/redirect.cc
+++ html-redirect/redirect.cc
@@ -3,47 +3,42 @@
 #include <filesystem>
 
 std::string redirect(const std::string& inputPath, const std::string& basePath) {
-  namespace fs = std::filesystem;
+    namespace fs = std::filesystem;
 
-  // Special case for root path
-  if (inputPath == "/") return "/index.html";
+    // Resolve the full path
+    fs::path fullPath = fs::path(basePath) / inputPath;
 
-  fs::path fullPath = fs::path(basePath) / inputPath;
+    // 1. Try the given path if it's a file
+    if (fs::is_regular_file(fullPath)) {
+        return fullPath.string().substr(basePath.length());
+    }
 
-  // 1. Try the given path if it's a file
-  if (fs::is_regular_file(fullPath)) {
-      return fullPath.string().substr(basePath.length());
-  }
+    // 2. Try appending a `/` to the path and checking for an index.html
+    fs::path indexPath = fullPath / "index.html";
+    if (fs::exists(indexPath)) {
+        return indexPath.string().substr(basePath.length());
+    }
 
-  // 2. Try appending a `/` to the path and checking for an index.html
-  fs::path indexPath = fullPath / "index.html";
-  if (fs::exists(indexPath)) {
-      return indexPath.string().substr(basePath.length());
-  }
+    // 3. Check if appending a .html file extension gives a valid file
+    fs::path htmlPath = fullPath;
+    htmlPath.replace_extension(".html");
+    if (fs::is_regular_file(htmlPath)) {
+        return htmlPath.string().substr(basePath.length());
+    }
 
-  // 3. Check if the input ends with .html, strip it and check
-  if (fullPath.extension() == ".html") {
-      fs::path strippedPath = fullPath;
-      strippedPath.replace_extension("");
-      if (fs::exists(strippedPath)) {
-          return strippedPath.string().substr(basePath.length());
-      }
-  }
+    // Special case for root path
+    if (inputPath == "/") {
+        fs::path rootIndexPath = fs::path(basePath) / "index.html";
+        return fs::exists(rootIndexPath) ? "/index.html" : "";
+    }
 
-  // 4. Check if a .html file exists for the given path
-  fs::path htmlPath = fullPath;
-  htmlPath.replace_extension(".html");
-  if (fs::exists(htmlPath)) {
-      return htmlPath.string().substr(basePath.length());
-  }
-
-  // If no valid path is found, return empty string
-  return "";
+    // If no valid path is found, return empty string
+    return "";
 }
 
 int main() {
-  std::string basePath = "/path/to/base"; // Replace this with your actual base path
-  std::string testPath = "/a-conflict-index"; // Example test path
-  std::string resolvedPath = redirect(testPath, basePath);
-  std::cout << resolvedPath << std::endl;
+    std::string basePath = "/path/to/base"; // Replace this with your actual base path
+    std::string testPath = "/an-index-file/a-html-file"; // Example test path
+    std::string resolvedPath = redirect(testPath, basePath);
+    std::cout << resolvedPath << std::endl;
 }
--- html-redirect/test-cases/1/test.js
+++ html-redirect/test-cases/1/test.js
@@ -6,11 +6,12 @@ import { redirect } from '../../redirect.js'
 const __dirname = desm(import.meta.url)
 
 test('index export', async (t) => {
-  // assert.strictEqual(await redirect('/', __dirname), '/index.html')
-  // assert.strictEqual(await redirect('/index.html', __dirname), '/index.html')
+  assert.strictEqual(await redirect('/', __dirname), '/index.html', '/index.html')
+  assert.strictEqual(await redirect('/index.html', __dirname), '/index.html')
   assert.strictEqual(await redirect('/a-conflict-index', __dirname), '/a-conflict-index/index.html')
-  // assert.strictEqual(await redirect('/another-file', __dirname), '/another-file.html')
-  // assert.strictEqual(await redirect('/an-index-file/', __dirname), '/an-index-file/index.html')
-  // assert.strictEqual(await redirect('/an-index-file', __dirname), '/an-index-file/index.html')
-  // assert.strictEqual(await redirect('/an-index-file/a-html-file', __dirname), '/an-index-file/.html')
+  assert.strictEqual(await redirect('/another-file', __dirname), '/another-file.html')
+  assert.strictEqual(await redirect('/another-file.html', __dirname), '/another-file.html')
+  assert.strictEqual(await redirect('/an-index-file/', __dirname), '/an-index-file/index.html')
+  assert.strictEqual(await redirect('/an-index-file', __dirname), '/an-index-file/index.html')
+  assert.strictEqual(await redirect('/an-index-file/a-html-file', __dirname), '/an-index-file/a-html-file.html')
 })
