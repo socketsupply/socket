4e32f83d3e3c3ec5a17a5e5e50f9aa778f642b8f
Bret Comnes <bcomnes@gmail.com>
2023-10-10 11:50:57 -0700

    Improve algorithm

--- html-redirect/redirect.cc
+++ html-redirect/redirect.cc
@@ -5,40 +5,43 @@
 std::string redirect(const std::string& inputPath, const std::string& basePath) {
     namespace fs = std::filesystem;
 
-    // Resolve the full path
     fs::path fullPath = fs::path(basePath) / inputPath;
 
-    // 1. Try the given path if it's a file
     if (fs::is_regular_file(fullPath)) {
-        return fullPath.string().substr(basePath.length());
+        return stripBasePath(fullPath.string(), basePath);
     }
 
-    // 2. Try appending a `/` to the path and checking for an index.html
     fs::path indexPath = fullPath / "index.html";
-    if (fs::exists(indexPath)) {
-        return indexPath.string().substr(basePath.length());
+    if (fs::is_regular_file(indexPath)) {
+        return stripBasePath(indexPath.string(), basePath);
     }
 
-    // 3. Check if appending a .html file extension gives a valid file
     fs::path htmlPath = fullPath;
     htmlPath.replace_extension(".html");
     if (fs::is_regular_file(htmlPath)) {
-        return htmlPath.string().substr(basePath.length());
+        return stripBasePath(htmlPath.string(), basePath);
     }
 
-    // Special case for root path
     if (inputPath == "/") {
         fs::path rootIndexPath = fs::path(basePath) / "index.html";
-        return fs::exists(rootIndexPath) ? "/index.html" : "";
+        return fs::is_regular_file(rootIndexPath) ? "/index.html" : "";
     }
 
-    // If no valid path is found, return empty string
     return "";
 }
 
-int main() {
-    std::string basePath = "/path/to/base"; // Replace this with your actual base path
-    std::string testPath = "/an-index-file/a-html-file"; // Example test path
-    std::string resolvedPath = redirect(testPath, basePath);
-    std::cout << resolvedPath << std::endl;
+// Helper function to strip the basePath from the fullPath
+std::string stripBasePath(const std::string& fullPath, const std::string& basePath) {
+    size_t pos = fullPath.find(basePath);
+    if (pos != std::string::npos) {
+        return fullPath.substr(pos + basePath.length());
+    }
+    return fullPath;
 }
+
+// int main() {
+//     std::string basePath = "/path/to/base"; // Replace this with your actual base path
+//     std::string testPath = "/an-index-file/a-html-file"; // Example test path
+//     std::string resolvedPath = redirect(testPath, basePath);
+//     std::cout << resolvedPath << std::endl;
+// }
