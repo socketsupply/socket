f93f9b908c5b761c84bf4720deef72fc71c31e08
Sergey Rubanov <chi187@gmail.com>
2023-10-11 22:13:04 +0200

    feat(ci): add socket-node version check

--- bin/ci_version_check.sh
+++ bin/ci_version_check.sh
@@ -8,10 +8,28 @@ VERSION_EXPECTED="$VERSION_TXT ($VERSION_GIT)"
 
 if [ "$VERSION_SSC" = "$VERSION_EXPECTED" ]; then
   echo "Version check has passed";
-  exit 0;
 else
   echo "Version check has failed";
   echo "Expected: $VERSION_EXPECTED";
   echo "Got: $VERSION_SSC";
   exit 1;
+fi
+
+BASE_LIST=($(echo $VERSION_TXT | tr '.' ' '))
+
+V_MAJOR=${BASE_LIST[0]}
+V_MINOR=${BASE_LIST[1]}
+
+VERSION_NODE=$(npm show ./npm/packages/@socketsupply/socket-node version)
+
+BASE_LIST=($(echo $VERSION_NODE | tr '.' ' '))
+
+V_MAJOR_NODE=${BASE_LIST[0]}
+V_MINOR_NODE=${BASE_LIST[1]}
+
+if [ "$V_MAJOR" -ne "$V_MAJOR_NODE" ] || [ "$V_MINOR" -ne "$V_MINOR_NODE" ]; then
+    echo "Version of @socketsupply/socket-node is not in sync with @socketsupply/socket";
+    echo "@socketsupply/socket version is $VERSION_TXT";
+    echo "@socketsupply/socket-node version is $VERSION_NODE";
+    exit 1;
 fi
