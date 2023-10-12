11adcc69089139f5b83eac6605a97da830d9a372
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2023-10-11 22:47:27 -0400

    fix(types): extend Window type with built-in events

--- api/index.d.ts
+++ api/index.d.ts
@@ -6583,3 +6583,25 @@ declare module "socket:test/harness" {
     import * as exports from "socket:test/harness";
     
 }
+type MenuItemSelection = {
+    title: string
+    parent: string
+    state: '0'
+}
+declare interface Window {
+    addEventListener(
+        type: "menuItemSelected",
+        listener: (event: CustomEvent<MenuItemSelection>) => void,
+        options?: boolean | AddEventListenerOptions
+    ): void;
+    addEventListener(
+        type: "process-error",
+        listener: (event: CustomEvent<string>) => void,
+        options?: boolean | AddEventListenerOptions
+    ): void;
+    addEventListener(
+        type: "backend-exit",
+        listener: (event: CustomEvent<string>) => void,
+        options?: boolean | AddEventListenerOptions
+    ): void;
+}
