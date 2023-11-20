fe034d1a1d1907d0aaf50094af7e4502a13fd6ae
Joseph Werle <joseph.werle@gmail.com>
2023-11-20 11:54:43 -0400

    chore(test): allow 'SOCKET_DEBUG_IPC' in mobile

--- test/scripts/test-ios-simulator.js
+++ test/scripts/test-ios-simulator.js
@@ -1,5 +1,5 @@
 import { execSync as exec } from 'node:child_process'
 
-exec('ssc build --headless --test=./index.js --platform=ios-simulator --prod -r -o', {
+exec('ssc build --headless --test=./index.js --platform=ios-simulator --prod -r -o --env SOCKET_DEBUG_IPC', {
   stdio: 'inherit'
 })
