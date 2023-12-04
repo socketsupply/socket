8b89fb30d2c97a5f0503d77578889c5225c78036
Joseph Werle <joseph.werle@gmail.com>
2023-12-04 16:08:15 +0100

    refactor(src/core/file_system_watcher.*): use 'GSource' GTK loop interface

--- src/core/file_system_watcher.cc
+++ src/core/file_system_watcher.cc
@@ -1,9 +1,57 @@
 #include "file_system_watcher.hh"
+#include "core.hh"
 
 namespace SSC {
+#if defined(__linux__) && !defined(__ANDROID__)
+  static void initializeUVSource (FileSystemWatcher* watcher) {
+    watcher->uvSourceFunctions.prepare = [](GSource *source, gint *timeout) -> gboolean {
+      auto watcher = reinterpret_cast<FileSystemWatcher::UVSource*>(source)->watcher;
+
+      if (watcher == nullptr) {
+        return false;
+      }
+
+      auto loop = watcher->loop;
+
+      if (loop == nullptr) {
+        return false;
+      }
+
+      if (!uv_loop_alive(loop) || !watcher->isRunning) {
+        return false;
+      }
+
+      *timeout = uv_backend_timeout(loop);
+
+      return *timeout == 0;
+    };
+
+    watcher->uvSourceFunctions.dispatch = [](
+      GSource *source,
+      GSourceFunc callback,
+      gpointer user_data
+    ) -> gboolean {
+      auto watcher = reinterpret_cast<FileSystemWatcher::UVSource*>(source)->watcher;
+
+      if (watcher == nullptr) {
+        return false;
+      }
+
+      auto loop = watcher->loop;
+
+      if (loop == nullptr) {
+        return false;
+      }
+
+      uv_run(loop, UV_RUN_NOWAIT);
+      return G_SOURCE_CONTINUE;
+    };
+  }
+#endif
+
   static FileSystemWatcher::Path resolveFileNameForContext (
-      const String& filename,
-      const FileSystemWatcher::Context* context
+    const String& filename,
+    const FileSystemWatcher::Context* context
   ) {
     if (context->isDirectory) {
       return FileSystemWatcher::Path(context->name) / filename;
@@ -17,6 +65,9 @@ namespace SSC {
 
     while (watcher->isRunning) {
       do {
+        uv_update_time(watcher->loop);
+        const auto timeout = uv_backend_timeout(watcher->loop);
+        msleep(timeout);
         uv_run(watcher->loop, UV_RUN_NOWAIT);
       } while (watcher->isRunning && uv_loop_alive(watcher->loop));
     }
@@ -62,10 +113,16 @@ namespace SSC {
 
   FileSystemWatcher::FileSystemWatcher (const String& path) {
     this->paths.push_back(path);
+  #if defined(__linux__) && !defined(__ANDROID__)
+    initializeUVSource(this);
+  #endif
   }
 
   FileSystemWatcher::FileSystemWatcher (const Vector<String>& paths) {
     this->paths = paths;
+  #if defined(__linux__) && !defined(__ANDROID__)
+    initializeUVSource(this);
+  #endif
   }
 
   FileSystemWatcher::~FileSystemWatcher () {
@@ -87,17 +144,30 @@ namespace SSC {
     }
 
     Lock lock(this->mutex);
-    this->isRunning = true;
     this->callback = callback;
 
     // a loop may be configured for the instance already, perhaps here or
     // manually by the caller
     if (this->loop == nullptr) {
       this->loop = uv_loop_new();
-      this->thread = new std::thread(&poll, this);
       if (uv_loop_init(this->loop) != 0) {
         return false;
       }
+
+    #if defined(__linux__) && !defined(__ANDROID__)
+      GSource *source = g_source_new(&this->uvSourceFunctions, sizeof(UVSource));
+      UVSource *uvSource = (UVSource *) source;
+      uvSource->watcher = this;
+      uvSource->tag = g_source_add_unix_fd(
+        source,
+        uv_backend_fd(this->loop),
+        (GIOCondition) (G_IO_IN | G_IO_OUT | G_IO_ERR)
+      );
+
+      g_source_attach(source, nullptr);
+    #else
+      this->thread = new std::thread(&poll, this);
+    #endif
     }
 
     for (const auto& path : this->paths) {
@@ -137,8 +207,8 @@ namespace SSC {
       );
     }
 
+    this->isRunning = true;
     return true;
-
   }
 
   bool FileSystemWatcher::stop () {
