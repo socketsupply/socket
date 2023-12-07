c0f410f71ecf9204b0a87889e5cd20d69db67f8a
Joseph Werle <joseph.werle@gmail.com>
2023-12-07 13:45:43 +0100

    refactor(src/ipc/bridge.cc): make 'http' secure when '--host' is given

--- src/ipc/bridge.cc
+++ src/ipc/bridge.cc
@@ -2630,6 +2630,13 @@ static void registerSchemeHandler (Router *router) {
   webkit_security_manager_register_uri_scheme_as_secure(security, "ipc");
   webkit_security_manager_register_uri_scheme_as_local(security, "ipc");
 
+  if (userConfig["host"].starts_with("http:")) {
+    webkit_security_manager_register_uri_scheme_as_display_isolated(security, "http");
+    webkit_security_manager_register_uri_scheme_as_cors_enabled(security, "http");
+    webkit_security_manager_register_uri_scheme_as_secure(security, "http");
+    webkit_security_manager_register_uri_scheme_as_local(security, "http");
+  }
+
   webkit_security_manager_register_uri_scheme_as_display_isolated(security, "socket");
   webkit_security_manager_register_uri_scheme_as_cors_enabled(security, "socket");
   webkit_security_manager_register_uri_scheme_as_secure(security, "socket");
