ebd91e8f64918ec43fb9f4fbc76e963d9ff28e57
Sergey Rubanov <chi187@gmail.com>
2023-12-12 16:56:09 +0100

    tests(test/src/webassembly.js): add WebAssembly tests

---
+++ test/src/webassembly.js
@@ -0,0 +1,30 @@
+import { test } from 'socket:test'
+import fs from 'socket:fs'
+import { createFile } from 'socket:fs/web'
+
+const importObject = {
+  env: {
+    myFunc: arg => arg * arg
+  }
+}
+
+test('load wasm synchronously', async (t) => {
+  const wasm = await fs.promises.readFile('./webassembly/program.wasm')
+  const module = new WebAssembly.Module(wasm)
+  const { exports } = new WebAssembly.Instance(module, importObject)
+  t.equal(exports.runMyFunc(5), 25, 'imported function works')
+  t.equal(exports.identity(42), 42, 'exported function works')
+})
+
+// test('load wasm asynchronously', async (t) => {
+//   const file = await createFile('./webassembly/program.wasm')
+//   const stream = file.stream()
+//   const response = new __native_Response(stream, {
+//     headers: {
+//       'Content-Type': 'application/wasm'
+//     }
+//   })
+//   const { exports } = await WebAssembly.instantiateStreaming(response, importObject)
+//   t.equal(exports.runMyFunc(5), 25, 'imported function works')
+//   t.equal(exports.identity(42), 42, 'exported function works')
+// })
