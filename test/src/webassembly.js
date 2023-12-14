57b56ce31bcc014a8f49e5d23197affcded3596a
Sergey Rubanov <chi187@gmail.com>
2023-12-14 14:52:44 +0100

    feat(wasm): add support for fetching local wasm modules

--- test/src/webassembly.js
+++ test/src/webassembly.js
@@ -8,7 +8,7 @@ const importObject = {
   }
 }
 
-test('load wasm synchronously', async (t) => {
+test('load wasm synchronously from FS', async (t) => {
   const wasm = await fs.promises.readFile('./webassembly/program.wasm')
   const module = new WebAssembly.Module(wasm)
   const { exports } = new WebAssembly.Instance(module, importObject)
@@ -16,10 +16,21 @@ test('load wasm synchronously', async (t) => {
   t.equal(exports.identity(42), 42, 'exported function works')
 })
 
-test('load wasm asynchronously', async (t) => {
+test('load wasm asynchronously from FS', async (t) => {
   const file = await createFile('./webassembly/program.wasm')
   const stream = file.stream()
   const response = new Response(stream, {
+    headers: {
+      'Content-Type': 'application/wasm'
+    }
+  })
+  const { instance } = await WebAssembly.instantiateStreaming(response, importObject)
+  t.equal(instance.exports.runMyFunc(5), 25, 'imported function works')
+  t.equal(instance.exports.identity(42), 42, 'exported function works')
+})
+
+test('load wasm asynchronously with fetch', async (t) => {
+  const response = fetch('./webassembly/program.wasm', {
     headers: {
       'Content-Type': 'application/wasm'
     }
