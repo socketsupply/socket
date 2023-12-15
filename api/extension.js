60639fd3640ae76903ec6bba7551f248315c50cf
Joseph Werle <joseph.werle@gmail.com>
2023-12-15 16:24:29 +0100

    refactor(api/extension.js): more libc and extension ABI impl

--- api/extension.js
+++ api/extension.js
@@ -9,6 +9,7 @@
  */
 import { isBufferLike } from './util.js'
 import { createFile } from './fs/web.js'
+// import { Buffer } from './buffer.js'
 import process from './process.js'
 import crypto from './crypto.js'
 import ipc from './ipc.js'
@@ -32,6 +33,159 @@ class WebAssemblyResponse extends Response {
   }
 }
 
+class WebAssemblyExtensionStack {
+  constructor (adapter) {
+    this.adapter = adapter
+    this.limits = {
+      min: this.adapter.instance.exports.__stack_low.value,
+      max: this.adapter.instance.exports.__stack_high.value
+    }
+
+    this.offset = this.limits.min
+    this.current = []
+  }
+
+  get buffer () {
+    return this.adapter.buffer
+  }
+
+  get (pointer) {
+    return this.buffer.subarray(pointer)
+  }
+
+  push (value) {
+    const pointer = this.offset
+
+    if (value === null) {
+      value = 0
+    }
+
+    if (typeof value === 'string') {
+      value = this.adapter.textEncoder.encode(value)
+    } else if (isBufferLike(value)) {
+      value = new Uint8Array(value)
+    }
+
+    if (!isBufferLike(value) && typeof value !== 'number') {
+      throw new TypeError(
+        'WebAssemblyExtensionStack: ' +
+        'Expected value to be TypedArray or a number: ' +
+        `Got ${typeof value}`
+      )
+    }
+
+    let byteLength = 0
+    const isFloat = typeof value === 'number' && parseInt(value) !== value
+
+    if (isBufferLike(value)) {
+      byteLength = value.byteLength
+    } else {
+      const zeroes = Math.clz32(value)
+      if (zeroes <= 8) {
+        byteLength = 4
+      } else if (zeroes <= 16) {
+        byteLength = 2
+      } else if (zeroes <= 32) {
+        byteLength = 1
+      }
+    }
+
+    this.current.push(byteLength)
+    this.offset += byteLength
+
+    if (isBufferLike(value)) {
+      this.adapter.set(pointer, value)
+    } else if (byteLength === 4) {
+      if (isFloat) {
+        this.adapter.setFloat32(pointer, value)
+      } else {
+        this.adapter.setInt32(pointer, value)
+      }
+    } else if (byteLength === 2) {
+      if (isFloat) {
+        this.adapter.setFloat16(pointer, value)
+      } else {
+        this.adapter.setInt16(pointer, value)
+      }
+    } else if (byteLength === 1) {
+      if (isFloat) {
+        this.adapter.setFloat8(pointer, value)
+      } else {
+        this.adapter.setInt8(pointer, value)
+      }
+    }
+
+    return pointer
+  }
+
+  pop () {
+    if (this.current.length === 0) {
+      return 0
+    }
+
+    const byteLength = this.current.pop()
+    const pointer = this.offset - byteLength
+    this.offset -= byteLength
+    return pointer
+  }
+
+  rewind (offset) {
+    const pointers = []
+    while (this.offset > offset) {
+      pointers.push(this.pop())
+    }
+    return pointers
+  }
+}
+
+class WebAssemblyExtensionHeap {
+  constructor (adapter) {
+    this.adapter = adapter
+  }
+
+  get buffer () {
+    return this.adapter.buffer
+  }
+
+  get (pointer) {
+    return this.buffer.subarray(pointer)
+  }
+}
+
+class WebAssemblyExtensionIndirectFunctionTable {
+  constructor (adapter, options = null) {
+    this.adapter = adapter
+    this.table = (
+      options?.table ??
+      adapter.instance?.exports?.__indirect_function_table ??
+      new WebAssembly.Table({ initial: 0, element: 'anyfunc' })
+    )
+  }
+
+  call (index, ...args) {
+    if (this.table.length === 0 || index >= this.table.length) {
+      return null
+    }
+
+    const callable = this.table.get(index)
+    const values = []
+    const mark = this.adapter.stack.offset
+
+    for (const arg of args) {
+      if (typeof arg === 'number') {
+        values.push(arg)
+      } else {
+        values.push(this.adapter.stack.push(arg))
+      }
+    }
+
+    const value = callable(...values)
+    this.adapter.stack.rewind(mark)
+
+    return value
+  }
+}
+
 /**
  * An adapter for reading and writing various values from a WebAssembly instance's
  * memory buffer.
@@ -47,69 +201,20 @@ class WebAssemblyExtensionAdapter {
     this.textDecoder = new TextDecoder()
     this.textEncoder = new TextEncoder()
 
-    this.heap = this.buffer.subarray(this.instance.exports.__heap_base.value)
-    this.stack = this.buffer.subarray(this.instance.exports.__stack_low.value)
-  }
-
-  get heapBaseOffset () {
-    return this.instance.exports.__heap_base.value
-  }
-
-  get stackBaseOffset () {
-    return this.instance.exports.__stack_low.value
-  }
-
-  get baseOffset () {
-    return this.instance.exports.__memory_base.value
-  }
-
-  get indirectFunctionTable () {
-    return this.instance?.exports?.__indirect_function_table ?? null
-  }
-
-  callIndirect (offset, ...args) {
-    const { indirectFunctionTable, stackBaseOffset } = this
-
-    if (!indirectFunctionTable || !indirectFunctionTable.length) {
-      return null
-    }
-
-    if (offset >= indirectFunctionTable.length) {
-      return null
-    }
-
-    const indirectFunction = indirectFunctionTable.get(offset)
-
-    if (!indirectFunction) {
-      return null
-    }
-
-    const values = []
-    let stackOffset = stackBaseOffset
-
-    for (const arg of args) {
-      if (typeof arg === 'string') {
-        const value = this.textEncoder.encode(arg)
-        this.buffer.set(value, stackOffset)
-        stackOffset += value.byteLength
-      } else if (isBufferLike(arg)) {
-        const value = new Uint8Array(arg)
-        this.buffer.set(value, stackOffset)
-        stackOffset += value.byteLength
-      } else if (typeof arg === 'number') {
-        values.push(arg)
-      }
-
-      values.push(stackOffset)
-    }
-
-    return indirectFunction(...values)
+    this.indirectFunctionTable = new WebAssemblyExtensionIndirectFunctionTable(this)
+    this.stack = new WebAssemblyExtensionStack(this)
+    this.heap = new WebAssemblyExtensionHeap(this)
   }
 
   init () {
+    this.instance.exports.__wasm_call_ctors()
     const offset = this.instance.exports.__sapi_extension_initializer()
     const context = new Uint8Array(1)
-    return Boolean(this.callIndirect(offset, context))
+    return Boolean(this.indirectFunctionTable.call(offset, context))
+  }
+
+  get globalBaseOffset () {
+    return this.instance.exports.__global_base.value
   }
 
   get (pointer) {
@@ -117,7 +222,7 @@ class WebAssemblyExtensionAdapter {
       return null
     }
 
-    return this.buffer.slice(pointer)
+    return this.buffer.subarray(pointer)
   }
 
   set (pointer, value) {
@@ -131,83 +236,59 @@ class WebAssemblyExtensionAdapter {
   }
 
   getFloat32 (pointer) {
-    return pointer ? this.view.getFloat32(pointer) : 0
+    return pointer ? this.view.getFloat32(pointer, true) : 0
   }
 
   setFloat32 (pointer, value) {
-    return pointer ? (this.view.setFloat32(pointer, value), true) : false
-  }
-
-  getFloat64 (pointer) {
-    return pointer ? this.view.getFloat64(pointer) : 0
-  }
-
-  setFloat64 (pointer, value) {
-    return pointer ? (this.view.setFloat64(pointer, value), true) : false
+    return pointer ? (this.view.setFloat32(pointer, value, true), true) : false
   }
 
   getInt8 (pointer) {
-    return pointer ? this.view.getInt8(pointer) : 0
+    return pointer ? this.view.getInt8(pointer, true) : 0
   }
 
   setInt8 (pointer, value) {
-    return pointer ? (this.view.setInt8(pointer, value), true) : false
+    return pointer ? (this.view.setInt8(pointer, value, true), true) : false
   }
 
   getInt16 (pointer) {
-    return pointer ? this.view.getInt16(pointer) : 0
+    return pointer ? this.view.getInt16(pointer, true) : 0
   }
 
   setInt16 (pointer, value) {
-    return pointer ? (this.view.setInt16(pointer, value), true) : false
+    return pointer ? (this.view.setInt16(pointer, value, true), true) : false
   }
 
   getInt32 (pointer) {
-    return pointer ? this.view.getInt32(pointer) : 0
+    return pointer ? this.view.getInt32(pointer, true) : 0
   }
 
   setInt32 (pointer, value) {
-    return pointer ? (this.view.setInt32(pointer, value), true) : false
-  }
-
-  getBigInt64 (pointer) {
-    return pointer ? this.view.getBigInt64(pointer) : 0
-  }
-
-  setBigInt64 (pointer, value) {
-    return pointer ? (this.view.setBigInt64(pointer, value), true) : false
+    return pointer ? (this.view.setInt32(pointer, value, true), true) : false
   }
 
   getUint8 (pointer) {
-    return pointer ? this.view.getUint8(pointer) : 0
+    return pointer ? this.view.getUint8(pointer, true) : 0
   }
 
   setUint8 (pointer, value) {
-    return pointer ? (this.view.setUint8(pointer, value), true) : false
+    return pointer ? (this.view.setUint8(pointer, value, true), true) : false
   }
 
   getUint16 (pointer) {
-    return pointer ? this.view.getUint16(pointer) : 0
+    return pointer ? this.view.getUint16(pointer, true) : 0
   }
 
   setUint16 (pointer, value) {
-    return pointer ? (this.view.setUint16(pointer, value), true) : false
+    return pointer ? (this.view.setUint16(pointer, value, true), true) : false
   }
 
   getUint32 (pointer) {
-    return pointer ? this.view.getUint32(pointer) : 0
+    return pointer ? this.view.getUint32(pointer, true) : 0
   }
 
   setUint32 (pointer, value) {
-    return pointer ? (this.view.setUint32(pointer, value), true) : false
-  }
-
-  getBigUint64 (pointer) {
-    return pointer ? this.view.getBigUint64(pointer) : 0
-  }
-
-  setBigUint64 (pointer, value) {
-    return pointer ? (this.view.setBigUint64(pointer, value), true) : false
+    return pointer ? (this.view.setUint32(pointer, value, true), true) : false
   }
 
   getString (pointer, buffer, size) {
@@ -220,7 +301,10 @@ class WebAssemblyExtensionAdapter {
       buffer = null
     }
 
-    const start = (buffer || this.buffer).slice(pointer)
+    const start = buffer
+      ? buffer.slice(pointer)
+      : this.stack.get(pointer)
+
     const end = size || start.indexOf(0) // NULL byte
 
     if (end === -1) {
@@ -296,9 +380,213 @@ function createWebAssemblyExtensionImports (env) {
     element: 'anyfunc'
   })
 
-  // libc
+  // <assert.h>
   Object.assign(imports.env, {
-    snprintf (buffer, size, format, ...args) {
+    __assert_fail (condition, filename, line, func) {
+    }
+  })
+
+  // <errno.h>
+  Object.assign(imports.env, {
+    __errno_location () {
+      // TODO(@jwerle): return pointer from `env.adapter.heap` that is in reserved space
+      return 0
+    }
+  })
+
+  function variadicFormatedStringFromPointers (
+    formatPointer,
+    variadicArguments = 0x0,
+    encoded = true
+  ) {
+    const format = env.adapter.getString(formatPointer)
+    if (!variadicArguments) {
+      if (encoded) {
+        return env.adapter.textEncoder.encode(format)
+      }
+
+      return format
+    }
+
+    const buffer = env.adapter.buffer.slice(variadicArguments)
+    const view = new DataView(buffer.buffer)
+
+    let index = 0
+
+    const regex = /%(l|ll|j|t|z)?(d|i|o|s|S|u|x|X|n|%)/g
+    const output = format.replace(regex, (x) => {
+      if (x === '%%') {
+        return '%'
+      }
+
+      switch (x) {
+        case '%S': case '%s': {
+          const pointer = view.getUint8((index++) * 4)
+          const offset = env.adapter.globalBaseOffset + pointer
+          return env.adapter.getString(offset, env.adapter.buffer)
+        }
+
+        case '%llu': case '%lu':
+        case '%lld': case '%ld':
+        case '%i': case '%x': case '%X': case '%d': {
+          return view.getInt32((index++) * 4, true)
+        }
+
+        case '%u': {
+          return view.getUint32((index++) * 4, true)
+        }
+      }
+
+      return x
+    })
+
+    if (encoded) {
+      return env.adapter.textEncoder.encode(output)
+    }
+
+    return output
+  }
+
+  // eslint-disable-next-line
+  const STDIN = 0x0
+  const STDOUT = 0x1
+  const STDERR = 0x2
+
+  const toBeFlushed = {
+    stdout: [],
+    stderr: []
+  }
+
+  // <stdio.h>
+  Object.assign(imports.env, {
+    printf (formatPointer, variadicArguments) {
+      return imports.env.fprintf(STDOUT, formatPointer, variadicArguments)
+    },
+
+    fprintf (descriptorPointer, formatPointer, variadicArguments) {
+      const output = variadicFormatedStringFromPointers(
+        formatPointer,
+        variadicArguments,
+        /* encoded = */ false
+      )
+
+      if (descriptorPointer === STDOUT) {
+        toBeFlushed.stdout.push(output)
+      } else if (descriptorPointer === STDERR) {
+        toBeFlushed.stderr.push(output)
+      }
+
+      if (output.includes('\n')) {
+        if (descriptorPointer === STDOUT) {
+          console.log(toBeFlushed.stdout.join(''))
+          toBeFlushed.stdout.splice(0, toBeFlushed.stdout.length)
+        } else if (descriptorPointer === STDERR) {
+          console.error(toBeFlushed.stderr.join(''))
+          toBeFlushed.stderr.splice(0, toBeFlushed.stderr.length)
+        }
+      }
+
+      return output.length
+    },
+
+    sprintf (stringPointer, formatPointer, variadicArguments) {
+      const output = variadicFormatedStringFromPointers(formatPointer, variadicArguments)
+
+      if (stringPointer !== 0) {
+        const buffer = env.adapter.get(stringPointer)
+        buffer.set(output)
+      }
+
+      return output.byteLength
+    },
+
+    snprintf (stringPointer, size, formatPointer, variadicArguments) {
+      const output = variadicFormatedStringFromPointers(formatPointer, variadicArguments)
+
+      if (stringPointer !== 0 && size !== 0) {
+        const buffer = env.adapter.get(stringPointer)
+        buffer.set(output.slice(0, size - 1))
+      }
+
+      return output.byteLength
+    }
+  })
+
+  // <string.h>
+  Object.assign(imports.env, {
+    memcpy (dst, src, size) {
+    },
+
+    memmove (dst, src, size) {
+    },
+
+    memset (dst, byte, size) {
+    },
+
+    memcmp (left, right, size) {
+    },
+
+    memchr (string, char, size) {
+    },
+
+    strcpy (dst, src) {
+    },
+
+    strncpy (dst, src, size) {
+    },
+
+    strcat (dst, src) {
+    },
+
+    strncat (dst, src, size) {
+    },
+
+    strcmp (left, right) {
+    },
+
+    strncmp (left, right, size) {
+    },
+
+    strcoll (left, right) {
+    },
+
+    strxfrm (dst, src, size) {
+    },
+
+    strchr (string, char) {
+    },
+
+    strrchr (string, char) {
+    },
+
+    strcspn (string, charset) {
+    },
+
+    strspn (string, charset) {
+    },
+
+    strpbrk (string, charset) {
+    },
+
+    strstr (string, needle) {
+    },
+
+    strtok (string, sep) {
+    },
+
+    strlen (string) {
+    },
+
+    strdup (string) {
+    },
+
+    strndup (string, size) {
+    },
+
+    strerror (errorCode) {
+    },
+
+    strsignal (signal) {
     }
   })
 
@@ -349,7 +637,7 @@ function createWebAssemblyExtensionImports (env) {
     sapi_context_dispatch (context, data, callback) {
       if (callback > 0) {
         process.nextTick(() => {
-          env.adapter.callIndirect(callback, context, data)
+          env.adapter.indirectFunctionTable.call(callback, context, data)
         })
       }
     },
@@ -365,10 +653,7 @@ function createWebAssemblyExtensionImports (env) {
         return 0
       }
 
-      const offset = env.adapter.stackBaseOffset
-      const value = env.adapter.textEncoder.encode(process.env[name])
-      env.adapter.buffer.set(value, offset)
-      return offset
+      return env.adapter.stack.push(process.env[name])
     },
 
     // javascript
@@ -377,7 +662,10 @@ function createWebAssemblyExtensionImports (env) {
       source = env.adapter.getString(source)
       // TODO(@jwerle): implement a 'vm' module and use it
       // eslint-disable-next-line
-      const evaluation = new Function(`return function ${name || ''} () { ${source}; }`)()
+      const evaluation = new Function(`
+        return function () { ${source}; }
+        //# sourceURL=${name}
+      `)()
       try {
         await evaluation()
       } catch (err) {
