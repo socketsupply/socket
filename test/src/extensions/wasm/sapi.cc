63d4381fddec53335e03373936bb55359e720617
Joseph Werle <joseph.werle@gmail.com>
2023-12-19 17:04:25 +0100

    refactor(test/src/extensions/wasm/sapi.cc): set header value

--- test/src/extensions/wasm/sapi.cc
+++ test/src/extensions/wasm/sapi.cc
@@ -11,6 +11,7 @@ static void onhello (
   auto string = sapi_json_string_create(context, "world");
   sapi_json_object_set(object, "hello", string);
   sapi_ipc_result_set_json_data(result, sapi_json_any(object));
+  sapi_ipc_result_set_header(result, "header-name", "header value");
   sapi_ipc_reply(result);
 }
 
