014ccfeb2e343fed8f11635f125946a6d5ad177e
Joseph Werle <joseph.werle@gmail.com>
2024-01-05 14:54:31 +0100

    refactor(include/socket/webassembly): more 'libc' prototypes

---
+++ include/socket/webassembly/err.h
@@ -0,0 +1,15 @@
+#ifndef SOCKET_RUNTIME_WEBASSEMBLY_ERR_H
+#define SOCKET_RUNTIME_WEBASSEMBLY_ERR_H
+
+#if !defined(_ERR_H)
+#define _ERR_H
+#endif
+
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
+#if defined(__cplusplus)
+}
+#endif
+#endif
---
+++ include/socket/webassembly/sys/param.h
@@ -0,0 +1,15 @@
+#ifndef SOCKET_RUNTIME_WEBASSEMBLY_SYS_PARAM_H
+#define SOCKET_RUNTIME_WEBASSEMBLY_SYS_PARAM_H
+
+#if !defined(_PARAM_H)
+#define _PARAM_H
+#endif
+
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
+#if defined(__cplusplus)
+}
+#endif
+#endif
