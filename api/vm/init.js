3f267b6c6b18303e66561a080b63c5827de2899c
Joseph Werle <joseph.werle@gmail.com>
2024-02-03 23:49:48 +0100

    refactor(api/vm): improve worker/realml/worker transfers

---
+++ api/vm/init.js
@@ -0,0 +1,171 @@
+import application from '../application.js'
+import * as vm from '../vm.js'
+import hooks from '../hooks.js'
+
+class World extends EventTarget {
+  ready = null
+  frame = null
+  state = null
+  id = null
+
+  constructor (state, id) {
+    super()
+    this.state = state
+    this.id = id
+    this.frame = createWorld({ id })
+    this.ready = new Promise((resolve) => {
+      this.frame.addEventListener('load', resolve, { once: true })
+    })
+  }
+
+  async postMessage (...args) {
+    await this.ready
+    this.frame.contentWindow.postMessage(...args)
+  }
+
+  async destroy () {
+    // send destroy signal
+    await this.postMessage({ id: this.id, type: 'destroy' })
+    // wait for state to emit 'destroyed' on instance, after GC
+    await new Promise((resolve) => {
+      this.addEventListener('destroyed', resolve, { once: true })
+    })
+  }
+}
+
+class State {
+  static init () {
+    const state = new State()
+    state.init()
+    return state
+  }
+
+  ports = []
+  worker = null
+
+  /**
+   * A mapping of client IDs to content worlds.
+   * @type {Map<String, World>}
+   */
+  worlds = new Map()
+  clients = new Map()
+
+  constructor () {
+    this.onMessage = this.onMessage.bind(this)
+    this.onWorkerMessage = this.onWorkerMessage.bind(this)
+    this.onWorkerMessageError = this.onWorkerMessageError.bind(this)
+  }
+
+  init () {
+    hooks.onReady(async () => {
+      this.worker = await vm.getContextWorker()
+      this.worker.port.addEventListener('message', this.onWorkerMessage)
+      this.worker.port.addEventListener('mesageerror', this.onWorkerMessageError)
+      this.worker.port.postMessage({ type: 'realm' })
+
+      const windows = await application.getWindows()
+      const currentWindow = await application.getCurrentWindow()
+
+      for (const index in windows) {
+        const window = windows[index]
+        if (window.index !== currentWindow.index) {
+          await currentWindow.send({
+            window: window.index,
+            event: `vm:${currentWindow.index}:ready`,
+            value: {}
+          })
+        }
+      }
+
+      globalThis.addEventListener('message', this.onMessage)
+    })
+  }
+
+  onMessage (event) {
+    if (event.data?.type === 'world.result') {
+      const transfer = []
+      vm.findMessageTransfers(transfer, event.data)
+      this.worker.port.postMessage({ ...event.data, type: 'result' }, { transfer })
+    }
+
+    if (event.data?.type === 'world.destroy') {
+      const { id } = event.data
+      const world = this.worlds.get(id)
+      if (world) {
+        world.dispatchEvent(new Event('destroyed'))
+        this.worlds.delete(id)
+        if (world.frame?.parentElement) {
+          world.frame.parentElement.removeChild(world.frame)
+        }
+      }
+    }
+  }
+
+  async onWorkerMessage (event) {
+    if (event.data?.type === 'terminate-worker') {
+      const pending = []
+      for (const world of this.worlds.values()) {
+        pending.push(world.destroy())
+      }
+
+      await Promise.all(pending)
+      const currentWindow = await application.getCurrentWindow()
+      await currentWindow.close()
+      this.worker = null
+    }
+
+    if (event.data?.type === 'script') {
+      const { id } = event.data
+      const transfer = []
+      const world = this.worlds.get(id) ?? new World(this, id)
+
+      if (!this.worlds.has(id)) {
+        this.worlds.set(id, world)
+      }
+
+      vm.findMessageTransfers(transfer, event.data)
+
+      world.postMessage(event.data, { transfer })
+    }
+
+    if (event.data?.type === 'destroy') {
+      const { id } = event.data
+      const world = this.worlds.get(id)
+      if (world) {
+        world.destroy()
+      }
+    }
+  }
+
+  onWorkerMessageError (event) {
+    console.error('onWorkerMessageError', event)
+  }
+}
+
+function createWorld (options) {
+  const frame = globalThis.document.createElement('iframe')
+
+  frame.setAttribute('sandbox', 'allow-same-origin allow-scripts')
+  frame.src = `${globalThis.origin}/socket/vm/world.html`
+  frame.id = options.id
+
+  Object.assign(frame.style, {
+    width: 0,
+    height: 0,
+    display: 'none'
+  })
+
+  const target = (
+    globalThis.document.head ??
+    globalThis.document.body ??
+    globalThis.document
+  )
+
+  target.appendChild(frame)
+
+  return frame
+}
+
+if (globalThis.window === globalThis) {
+  State.init()
+}
---
+++ api/vm/worker.js
@@ -0,0 +1,209 @@
+const Uint8ArrayPrototype = Uint8Array.prototype
+const TypedArrayPrototype = Object.getPrototypeOf(Uint8ArrayPrototype)
+const TypedArray = TypedArrayPrototype.constructor
+
+// eslint-disable-next-line
+function reportError (err) {
+  if (typeof globalThis.reportError === 'function') {
+    globalThis.reportError(err)
+  }
+}
+
+function isTypedArray (object) {
+  return object instanceof TypedArray
+}
+
+function isArrayBuffer (object) {
+  return object instanceof ArrayBuffer
+}
+
+function findMessageTransfers (transfers, object) {
+  if (isTypedArray(object) || ArrayBuffer.isView(object)) {
+    transfers.push(object.buffer)
+  } else if (isArrayBuffer(object)) {
+    transfers.push(object)
+  } else if (object instanceof MessagePort) {
+    transfers.push(object)
+  } else if (Array.isArray(object)) {
+    for (const value of object) {
+      findMessageTransfers(transfers, value)
+    }
+  } else if (object && typeof object === 'object') {
+    for (const key in object) {
+      findMessageTransfers(transfers, object[key])
+    }
+  }
+
+  return transfers
+}
+
+class Client extends EventTarget {
+  id = null
+  port = null
+
+  constructor (id, port) {
+    super()
+    this.id = id
+    this.port = port
+    this.onMessage = this.onMessage.bind(this)
+    this.port.addEventListener('message', this.onMessage)
+  }
+
+  onMessage (event) {
+    this.dispatchEvent(event)
+  }
+
+  postMessage (...args) {
+    return this.port.postMessage(...args)
+  }
+}
+
+class Realm {
+  /**
+   * The `MessagePort` for the VM realm.
+   * @type {MessagePort}
+   */
+  port = null
+
+  /**
+   * A reference to the top level worker statae
+   * @type {State}
+   */
+  state = null
+
+  /**
+   * Known content worlds that exist in a realm
+   * @type {Map<String, World>}
+   */
+  worlds = new Map()
+
+  constructor (state, port) {
+    this.state = state
+    this.port = port
+  }
+
+  get clients () {
+    return this.state.clients
+  }
+
+  postMessage (...args) {
+    return this.port.postMessage(...args)
+  }
+}
+
+class State {
+  static init () {
+    const state = new this()
+    state.init()
+    return state
+  }
+
+  /**
+   * All known connected `MessagePort` instances
+   * @type {MessagePort[]}
+   */
+  ports = []
+
+  /**
+   * Pending events to be dispatched to realm
+   * @type {MessageEvent[]}
+   */
+  pending = []
+
+  /**
+   * The realm for all virtual machines. This is a headless webview
+   */
+  realm = null
+
+  clients = new Map()
+
+  constructor () {
+    this.onConnect = this.onConnect.bind(this)
+  }
+
+  init () {
+    globalThis.addEventListener('connect', this.onConnect)
+  }
+
+  onConnect (event) {
+    for (const port of event.ports) {
+      this.ports.push(port)
+      port.start()
+      port.addEventListener('message', this.onPortMessage.bind(this, port))
+      port.postMessage('VM_SHARED_WORKER_ACK')
+    }
+  }
+
+  onPortMessage (port, event) {
+    // debug echo
+    // port.postMessage(event.data)
+
+    if (event.data?.type === 'terminate-worker') {
+      for (const port of this.ports) {
+        try {
+          port.postMessage({ type: 'terminate-worker' })
+        } catch {}
+      }
+
+      // timeout so realm can GC
+      setTimeout(() => {
+        globalThis.close()
+      }, 2000)
+      return
+    }
+
+    if (event.data?.type === 'client') {
+      const { id } = event.data
+      let client = null
+
+      if (this.clients.has(id)) {
+        client = this.clients.get(id)
+      } else {
+        client = new Client(id, port)
+        this.clients.set(id, client)
+      }
+    }
+
+    if (event.data?.type === 'script' || event.data?.type === 'destroy') {
+      const { id } = event.data
+      const client = this.clients.get(id)
+
+      if (client) {
+        if (!this.realm) {
+          this.pending.push(event)
+        } else {
+          const transfer = []
+          findMessageTransfers(transfer, event.data)
+          this.realm.postMessage(event.data, { transfer })
+        }
+      }
+    }
+
+    if (event.data?.type === 'result') {
+      const { id } = event.data
+      const client = this.clients.get(id)
+
+      if (client) {
+        const transfer = []
+        findMessageTransfers(transfer, event.data)
+        client.postMessage(event.data, { transfer })
+      }
+    }
+
+    if (event.data?.type === 'realm') {
+      this.realm = new Realm(this, port)
+      if (this.pending.length) {
+        for (const pendingEvent of this.pending) {
+          const transfer = []
+          findMessageTransfers(transfer, pendingEvent.data)
+          this.realm.postMessage(pendingEvent.data, { transfer })
+        }
+        this.pending.splice(0, this.pending.length)
+      }
+    }
+  }
+}
+
+if (globalThis.self && !globalThis.window) {
+  State.init()
+}
