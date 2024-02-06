a29e0ade5ab60b19d1a9858de675a403835bed26
Joseph Werle <joseph.werle@gmail.com>
2024-02-06 23:22:43 +0100

    refactor(api/window/hotkey.js): improve global interface

--- api/window/hotkey.js
+++ api/window/hotkey.js
@@ -1,23 +1,8 @@
-/* global HotKeyEvent, EventTarget */
+/* global EventTarget, HotKeyEvent, reportError */
+import hooks from '../hooks.js'
 import ipc from '../ipc.js'
+import gc from '../gc.js'
 import os from '../os.js'
-
-if (!/android|ios/.test(os.platform())) {
-  globalThis.addEventListener('hotkey', onHotKey)
-}
-
-/**
- * Global `HotKeyEvent` event listener for `Binding` instance event dispatch.
- * @ignore
- * @param {import('../internal/events.js').HotKeyEvent} event
- */
-function onHotKey (event) {
-  const { id } = event.data ?? {}
-  const binding = bindings.get(id)?.deref?.()
-  if (binding) {
-    binding.dispatchEvent(new HotKeyEvent('hotkey', event.data))
-  }
-}
 
 /**
  * Normalizes an expression string.
@@ -25,14 +10,226 @@ function onHotKey (event) {
  * @return {string}
  */
 export function normalizeExpression (expression) {
-  return encodeURIComponent(expression.split('+').map((token) => token.trim()).join('+'))
+  return encodeURIComponent(expression
+    .split('+')
+    .map((token) => token.trim())
+    .join('+')
+  )
 }
 
 /**
- * A map of weakly held `Binding` instances.
- * @type {Map<number, WeakRef<Binding>>}
+ * A high level bindings container map that dispatches events.
  */
-export const bindings = new Map()
+export class Bindings extends EventTarget {
+  /**
+   * A map of weakly held `Binding` instances.
+   * @type {Map<number, WeakRef<Binding>>}
+   */
+  #map = new Map()
+
+  /**
+   * The source `EventTarget` to listen for 'hotkey' events on
+   * @type {EventTarget}
+   */
+  #sourceEventTarget = null
+
+  /**
+   * `Bindings` class constructor.
+   * @ignore
+   * @param {EventTarget} [sourceEventTarget]
+   */
+  constructor (sourceEventTarget = globalThis) {
+    super()
+
+    this.onHotKey = this.onHotKey.bind(this)
+    this.#sourceEventTarget = sourceEventTarget
+
+    if (!/android|ios/.test(os.platform())) {
+      sourceEventTarget.addEventListener('hotkey', this.onHotKey)
+    }
+
+    gc.ref(this)
+    this.init().catch(reportError)
+  }
+
+  /**
+   * Intializes bindings
+   */
+  async init () {
+    for (const binding of await getBindings()) {
+      await binding.bind()
+    }
+  }
+
+  /**
+   * Implements `gc.finalizer` for gc'd resource cleanup.
+   * @return {gc.Finalizer}
+   * @ignore
+   */
+  [gc.finalizer] () {
+    return {
+      args: [this.#map, this.#sourceEventTarget, this.onHotKey],
+      handle (map, sourceEventTarget, onHotKey) {
+        map.clear()
+        if (sourceEventTarget) {
+          sourceEventTarget.removeEventListener('hotkey', onHotKey)
+        }
+      }
+    }
+  }
+
+  /**
+   * The number of `Binding` instances in the mapping.
+   * @type {number}
+   */
+  get size () {
+    return this.#map.size
+  }
+
+  /**
+   * Global `HotKeyEvent` event listener for `Binding` instance event dispatch.
+   * @ignore
+   * @param {import('../internal/events.js').HotKeyEvent} event
+   */
+  onHotKey (event) {
+    const { id } = event.data ?? {}
+    const binding = this.get(id)
+
+    if (binding) {
+      binding.dispatchEvent(new HotKeyEvent('hotkey', event.data))
+    }
+
+    const e = new HotKeyEvent('hotkey', event.data)
+    e.binding = binding ?? null
+    this.dispatchEvent(e)
+  }
+
+  /**
+   * Get a binding by `id`
+   * @param {number} id
+   * @return {Binding}
+   */
+  get (id) {
+    return this.#map.get(id)?.deref?.()
+  }
+
+  /**
+   * Set a `binding` a by `id`.
+   * @param {number} id
+   * @param {Binding} binding
+   */
+  set (id, binding) {
+    this.#map.set(id, new WeakRef(binding))
+  }
+
+  /**
+   * Delete a binding by `id`
+   * @param {number} id
+   * @return {boolean}
+   */
+  delete (id) {
+    return this.#map.delete(id)
+  }
+
+  /**
+   * Returns `true` if a binding existss in the mapping, otherwise `false`.
+   * @return {boolean}
+   */
+  has (id) {
+    return this.#map.has(id)
+  }
+
+  /**
+   * Known `Binding` values in the mapping.
+   * @return {{ next: function(): { value: Binding|undefined, done: boolean } }}
+   */
+  values () {
+    const values = Array
+      .from(this.#map.values())
+      .map((ref) => ref.deref() ?? null)
+      .filter(Boolean)
+    return values[Symbol.iterator]()
+  }
+
+  /**
+   * Known `Binding` keys in the mapping.
+   * @return {{ next: function(): { value: number|undefined, done: boolean } }}
+   */
+  keys () {
+    return this.#map.keys()
+  }
+
+  /**
+   * Known `Binding` ids in the mapping.
+   * @return {{ next: function(): { value: number|undefined, done: boolean } }}
+   */
+  ids () {
+    return this.keys()
+  }
+
+  /**
+   * Known `Binding` ids and values in the mapping.
+   * @return {{ next: function(): { value: [number, Binding]|undefined, done: boolean } }}
+   */
+  entries () {
+    const entries = Array
+      .from(this.#map.entries())
+      .map(([id, ref]) => [id, ref.deref() ?? null])
+      .filter((entry) => entry[1])
+    return entries[Symbol.iterator]()
+  }
+
+  /**
+   * Bind a global hotkey expression.
+   * @param {string} expression
+   * @return {Promise<Binding>}
+   */
+  async bind (expression) {
+    return await bind(expression)
+  }
+
+  /**
+   * Bind a global hotkey expression.
+   * @param {string} expression
+   * @return {Promise<Binding>}
+   */
+  async unbind (expression) {
+    return await unbind(expression)
+  }
+
+  /**
+   * Returns an array of all active bindings for the application.
+   * @return {Promise<Binding[]>}
+   */
+  async active () {
+    return await getBindings()
+  }
+
+  /**
+   * Resets all active bindings in the application.
+   * @param {boolean=} [currentContextOnly]
+   * @return {Promise}
+   */
+  async reset (currentContextOnly = false) {
+    if (currentContextOnly) {
+      for (const binding of this.values()) {
+        await binding.unbind()
+      }
+    } else {
+      const active = await this.active()
+      for (const binding of active) {
+        await binding.unbind()
+      }
+    }
+  }
+}
+
+/**
+ * A container for all the bindings currently bound
+ * by this window context.
+ * @type {Bindings}
+ */
+export const bindings = new Bindings()
 
 /**
  * An `EventTarget` container for a hotkey binding.
@@ -90,7 +287,7 @@ export class Binding extends EventTarget {
     }
 
     if (this.isValid) {
-      bindings.set(this.id, new WeakRef(this))
+      bindings.set(this.id, this)
     }
   }
 
@@ -135,12 +332,19 @@ export class Binding extends EventTarget {
   }
 
   /**
+   * Binds this hotkey expression.
+   * @return {Promise<Binding>}
+   */
+  async bind () {
+    return await bind(this.id)
+  }
+
+  /**
    * Unbinds this hotkey expression.
-   * @param {object=} poptions]
    * @return {Promise}
    */
-  async unbind (options = null) {
-    return await unbind(this.id, options)
+  async unbind () {
+    await unbind(this.id)
   }
 }
 
@@ -155,6 +359,19 @@ export async function bind (expression, options = null) {
     throw new TypeError(`The HotKey API is not supported on '${os.platform()}'`)
   }
 
+  await hooks.wait('ready')
+
+  if (typeof expression === 'number') {
+    const id = /** @type {number} */ (expression)
+    const result = await ipc.request('window.hotkey.bind', { id }, options)
+
+    if (result.err) {
+      throw result.err
+    }
+
+    return bindings.get(result.data.id) ?? new Binding(result.data)
+  }
+
   expression = normalizeExpression(expression)
   const result = await ipc.request('window.hotkey.bind', { expression }, options)
 
@@ -162,7 +379,7 @@ export async function bind (expression, options = null) {
     throw result.err
   }
 
-  return bindings.get(result.data.id)?.deref() ?? new Binding(result.data)
+  return bindings.get(result.data.id) ?? new Binding(result.data)
 }
 
 /**
@@ -173,6 +390,8 @@ export async function bind (expression, options = null) {
  */
 export async function unbind (id, options = null) {
   let result = null
+
+  await hooks.wait('ready')
 
   if (/android|ios/.test(os.platform())) {
     throw new TypeError(`The HotKey API is not supported on '${os.platform()}'`)
@@ -200,6 +419,8 @@ export async function unbind (id, options = null) {
  * @return {Promise<Binding[]>}
  */
 export async function getBindings (options = null) {
+  await hooks.wait('ready')
+
   if (/android|ios/.test(os.platform())) {
     throw new TypeError(`The HotKey API is not supported on '${os.platform()}'`)
   }
@@ -212,7 +433,7 @@ export async function getBindings (options = null) {
 
   if (Array.isArray(result.data)) {
     return result.data.map((data) =>
-      bindings.get(data.id)?.deref?.() ?? new Binding(data)
+      bindings.get(data.id) ?? new Binding(data)
     )
   }
 
@@ -226,6 +447,8 @@ export async function getBindings (options = null) {
  * @return {Promise<{ keys: object, modifiers: object }>}
  */
 export async function getMappings (options = null) {
+  await hooks.wait('ready')
+
   if (/android|ios/.test(os.platform())) {
     throw new TypeError(`The HotKey API is not supported on '${os.platform()}'`)
   }
@@ -239,7 +462,30 @@ export async function getMappings (options = null) {
   return result.data ?? {}
 }
 
+/**
+ * Adds an event listener to the global active bindings.
+ * @param {string} type
+ * @param {function(Event)} listener
+ * @param {(boolean|object)=} [optionsOrUseCapture]
+ */
+export function addEventListener (type, listener, optionsOrUseCapture) {
+  return bindings.addEventListener(type, listener, optionsOrUseCapture)
+}
+
+/**
+ * Removes  an event listener to the global active bindings.
+ * @param {string} type
+ * @param {function(Event)} listener
+ * @param {(boolean|object)=} [optionsOrUseCapture]
+ */
+export function removeEventListener (type, listener, optionsOrUseCapture) {
+  return bindings.removeEventListener(type, listener, optionsOrUseCapture)
+}
+
 export default {
   bind,
-  unbind
+  unbind,
+  bindings,
+  addEventListener,
+  removeEventListener
 }
