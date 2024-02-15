9d26fd3fffbb55bff007dc4a7a5a7c9dd528f52b
Joseph Werle <joseph.werle@gmail.com>
2024-02-15 16:38:44 +0100

    refactor(src/android): use module based preload, set client ID

--- src/android/webview.kt
+++ src/android/webview.kt
@@ -511,7 +511,7 @@ export default module
       }
 
       var html = String(assetStream.readAllBytes())
-      val script = """<script type="text/javascript">$preload</script>"""
+      val script = """<script type="module">$preload</script>"""
       val stream = java.io.PipedOutputStream()
       val response = android.webkit.WebResourceResponse(
         "text/html",
