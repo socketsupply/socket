f459b4f2683c84d0054420ed7cbd95a9d491d457
heapwolf <paolo@socketsupply.co>
2024-02-20 23:39:03 +0100

    refactor(api): implements child process

--- api/child_process/worker.js
+++ api/child_process/worker.js
@@ -1,5 +1,5 @@
 import ipc from '../ipc.js'
-import { isMainThread, parentPort } from '../worker_threads.js'
+import { parentPort } from '../worker_threads.js'
 import process from 'socket:process'
 
 const state = {}
@@ -15,39 +15,55 @@ const propagateWorkerError = err => parentPort.postMessage({
   }
 })
 
-parentPort.onmessage = ({ data: { id, method, args } }) => {
+parentPort.onmessage = async ({ data: { id, method, args } }) => {
   if (method === 'spawn') {
     const command = args[0]
     const argv = args[1]
     const opts = args[2]
 
     const params = {
-      args: Array.from(argv ?? []).join('\u0001'),
-      command,
-      cwd: opts?.cwd
+      args: [command, ...Array.from(argv ?? [])].join('\u0001'),
+      id,
+      cwd: opts?.cwd ?? ''
     }
 
     const { data, err } = await ipc.send('child_process.spawn', params)
-    
+
     if (err) return propagateWorkerError(err)
-    
-    state.id = data.id
+
+    state.id = BigInt(data.id)
     state.pid = data.pid
 
     parentPort.postMessage({ method: 'state', args: [state] })
-    
+
     globalThis.addEventListener('data', ({ detail }) => {
       const { err, data, source } = detail.params
       const buffer = detail.data
 
       if (err && err.id === state.id) {
-        return propagateWokerError(err)
+        return propagateWorkerError(err)
       }
 
       if (!data || BigInt(data.id) !== state.id) return
 
-      if (source === 'child_process.spawn') {
-        process.stdout.write(data)
+      if (source === 'child_process.spawn' && data.source === 'stdout') {
+        process.stdout.write(buffer)
+      }
+
+      if (source === 'child_process.spawn' && data.source === 'stderr') {
+        process.stderr.write(buffer)
+      }
+
+      if (source === 'child_process.spawn' && data.status === 'close') {
+        state.exitCode = data.code
+        state.lifecycle = 'close'
+        parentPort.postMessage({ method: 'state', args: [state] })
+      }
+
+      if (source === 'child_process.spawn' && data.status === 'exit') {
+        state.exitCode = data.code
+        state.lifecycle = 'exit'
+        parentPort.postMessage({ method: 'state', args: [state] })
       }
     })
   }
