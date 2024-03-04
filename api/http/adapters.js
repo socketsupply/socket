447405aae595e374272d540248426d155ee42c95
Joseph Werle <joseph.werle@gmail.com>
2024-03-04 14:19:26 +0000

    refactor(api/http): improve adapter, API, and https module

--- api/http/adapters.js
+++ api/http/adapters.js
@@ -1,11 +1,21 @@
 import { Deferred } from '../async.js'
+import { Buffer } from '../buffer.js'
 import assert from '../assert.js'
 
 /**
  * @typedef {{
+ *   Connection: typeof import('../http.js').Connection,
+ *   globalAgent: import('../http.js').Agent,
+ *   IncomingMessage: typeof import('../http.js').IncomingMessage,
+ *   ServerResponse: typeof import('../http.js').ServerResponse,
+ *   STATUS_CODES: object,
+ *   METHODS: string[]
  * }} HTTPModuleInterface
  */
 
+/**
+ * An abstract base clase for a HTTP server adapter.
+ */
 export class ServerAdapter extends EventTarget {
   #server = null
   #httpInterface = null
@@ -40,35 +50,49 @@ export class ServerAdapter extends EventTarget {
   get httpInterface () {
     return this.#httpInterface
   }
+
+  /**
+   * Called when the adapter should destroy itself.
+   * @abstract
+   */
+  async destroy () {}
 }
 
 /**
+ * A HTTP adapter for running a HTTP server in a service worker that uses the
+ * "fetch" event for the request and response lifecycle.
  */
 export class ServiceWorkerServerAdapter extends ServerAdapter {
   /**
    * `ServiceWorkerServerAdapter` class constructor.
    * @ignore
    * @param {import('../http.js').Server} server
+   * @param {HTTPModuleInterface} httpInterface
    */
-  constructor (server) {
+  constructor (server, httpInterface) {
     assert(
       globalThis.isServiceWorkerScope,
       'The ServiceWorkerServerAdapter can only be used in a ServiceWorker scope'
     )
 
-    super(server)
+    super(server, httpInterface)
 
-    globalThis.addEventListener('install', (event) => {
-      this.onInstall(event)
-    }, { once: true })
+    this.onInstall = this.onInstall.bind(this)
+    this.onActivate = this.onActivate.bind(this)
+    this.onFetch = this.onFetch.bind(this)
 
-    globalThis.addEventListener('activate', (event) => {
-      this.onActivate(event)
-    }, { once: true })
+    globalThis.addEventListener('install', this.onInstall, { once: true })
+    globalThis.addEventListener('activate', this.onActivate, { once: true })
+    globalThis.addEventListener('fetch', this.onFetch)
+  }
 
-    globalThis.addEventListener('fetch', (event) => {
-      this.onFetch(event)
-    })
+  /**
+   * Called when the adapter should destroy itself.
+   */
+  async destroy () {
+    globalThis.removeEventListener('install', this.onInstall, { once: true })
+    globalThis.removeEventListener('activate', this.onActivate, { once: true })
+    globalThis.removeEventListener('fetch', this.onFetch)
   }
 
   /**
@@ -94,7 +118,83 @@ export class ServiceWorkerServerAdapter extends ServerAdapter {
    * @param {import('../service-worker/events.js').FetchEvent}
    */
   async onFetch (event) {
-    const { request, context } = event
+    console.log('fetch', event)
+    if (this.server.closed) {
+      console.log('server closed')
+      return
+    }
+
+    const url = new URL(event.request.url)
+
+    if (this.server.port !== 0 && url.port !== this.server.port) {
+      console.log('port mismatch')
+      return
+    }
+
+    if (this.server.host && url.hostname !== this.server.host) {
+      console.log('host mismatch', { url: url.hostname, server: this.server.host })
+      return
+    }
+
+    if (this.server.connections.length >= this.server.maxConnections) {
+      console.log('too many connections')
+      event.respondWith(new Response())
+      return
+    }
+
+    const deferred = new Deferred()
+
+    const incomingMessage = new this.httpInterface.IncomingMessage({
+      complete: !/post|put/i.test(event.request.method),
+      headers: event.request.headers,
+      method: event.request.method,
+      url: event.request.url
+    })
+
+    const serverResponse = new this.httpInterface.ServerResponse({
+      request: incomingMessage
+    })
+
+    const connection = new this.httpInterface.Connection(
+      this.server,
+      incomingMessage,
+      serverResponse
+    )
+
+    this.server.emit('connection', connection)
+    event.respondWith(deferred.promise)
+
+    this.server.emit('request', incomingMessage, serverResponse)
+
+    if (/post|put/i.test(event.request.method)) {
+      const { highWaterMark } = incomingMessage._readableState
+      const requestArrayBuffer = await event.request.arrayBuffer()
+      const buffer = Buffer.from(requestArrayBuffer)
+      for (let i = 0; i < buffer.byteLength; i += highWaterMark) {
+        incomingMessage.push(buffer.slice(i, i + highWaterMark))
+      }
+    }
+
+    serverResponse.on('finish', () => {
+      const buffer = Buffer.concat(serverResponse.buffers)
+
+      if (!serverResponse.hasHeader('content-length')) {
+        serverResponse.setHeader('content-length', buffer.byteLength)
+      }
+
+      // disable cache for error responses
+      if (serverResponse.statusCode >= 400) {
+        serverResponse.setHeader('cache-control', 'no-cache')
+      }
+
+      const response = new Response(buffer, {
+        status: serverResponse.statusCode,
+        statusText: serverResponse.statusText,
+        headers: serverResponse.headers
+      })
+
+      deferred.resolve(response)
+    })
   }
 }
 
--- api/https.js
+++ api/https.js
@@ -1,20 +1,105 @@
 import http from './http.js'
+// re-export
+import * as exports from './http.js'
 
+/**
+ * All known possible HTTP methods.
+ * @type {string[]}
+ */
 export const METHODS = http.METHODS
+
+/**
+ * A mapping of status codes to status texts
+ * @type {object}
+ */
 export const STATUS_CODES = http.STATUS_CODES
 
+/**
+ * An options object container for an `Agent` instance.
+ */
 export class AgentOptions extends http.AgentOptions {}
 
+/**
+ * An Agent is responsible for managing connection persistence
+ * and reuse for HTTPS clients.
+ * @see {@link https://nodejs.org/api/https.html#class-httpsagent}
+ */
 export class Agent extends http.Agent {
   defaultProtocol = 'https:'
 }
 
-export class OutgoingMessage extends http.OutgoingMessage {}
+/**
+ * An object that is created internally and returned from `request()`.
+ * @see {@link https://nodejs.org/api/http.html#class-httpclientrequest}
+ */
 export class ClientRequest extends http.ClientRequest {}
+
+/**
+ * The parent class of `ClientRequest` and `ServerResponse`.
+ * It is an abstract outgoing message from the perspective of the
+ * participants of an HTTP transaction.
+ * @see {@link https://nodejs.org/api/http.html#class-httpoutgoingmessage}
+ */
+export class OutgoingMessage extends http.OutgoingMessage {}
+
+/**
+ * An `IncomingMessage` object is created by `Server` or `ClientRequest` and
+ * passed as the first argument to the 'request' and 'response' event
+ * respectively.
+ * It may be used to access response status, headers, and data.
+ * @see {@link https://nodejs.org/api/http.html#class-httpincomingmessage}
+ */
+export class IncomingMessage extends http.IncomingMessage {}
+
+/**
+ * An object that is created internally by a `Server` instance, not by the user.
+ * It is passed as the second parameter to the 'request' event.
+ * @see {@link https://nodejs.org/api/http.html#class-httpserverresponse}
+ */
 export class ServerResponse extends http.ServerResponse {}
 
+/**
+ * A duplex stream between a HTTP request `IncomingMessage` and the
+ * response `ServerResponse`
+ */
+export class Connection extends http.Connection {}
+
+/**
+ * A nodejs compat HTTP server typically intended for running in a "worker"
+ * environment.
+ * @see {@link https://nodejs.org/api/http.html#class-httpserver}
+ */
+export class Server extends http.Server {
+  /**
+   * The adapter interface for this `Server` instance.
+   * @ignore
+   */
+  get adapterInterace () {
+    return {
+      Connection,
+      globalAgent,
+      IncomingMessage,
+      METHODS,
+      ServerResponse,
+      STATUS_CODES
+    }
+  }
+}
+
+/**
+ * The global and default HTTPS agent.
+ * @type {Agent}
+ */
 export const globalAgent = new Agent()
 
+/**
+ * Makes a HTTPS request, optionally a `socket://` for relative paths when
+ * `socket:` is the origin protocol.
+ * @param {string|object} optionsOrURL
+ * @param {(object|function)=} [options]
+ * @param {function=} [callback]
+ * @return {ClientRequest}
+ */
 export function request (optionsOrURL, options, callback) {
   if (typeof optionsOrURL === 'string') {
     options = { Agent, ...options }
@@ -26,19 +111,28 @@ export function request (optionsOrURL, options, callback) {
   return http.request(optionsOrURL, options, callback)
 }
 
+/**
+ * Makes a HTTPS or `socket:` GET request. A simplified alias to `request()`.
+ * @param {string|object} optionsOrURL
+ * @param {(object|function)=} [options]
+ * @param {function=} [callback]
+ * @return {ClientRequest}
+ */
 export function get (optionsOrURL, options, callback) {
   return request(optionsOrURL, options, callback)
 }
 
-export default {
-  METHODS,
-  STATUS_CODES,
-  AgentOptions,
-  Agent,
-  globalAgent,
-  request,
-  OutgoingMessage,
-  ClientRequest,
-  ServerResponse,
-  get
+/**
+ * Creates a HTTPS server that can listen for incoming requests.
+ * Requests that are dispatched to this server depend on the context
+ * in which it is created, such as a service worker which will use a
+ * "fetch event" adapter.
+ * @param {object|function=} [options]
+ * @param {function=} [callback]
+ * @return {Server}
+ */
+export function createServer (...args) {
+  return http.createServer(...args)
 }
+
+export default exports
