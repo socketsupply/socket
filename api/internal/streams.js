298f6c2dd3089262a01a8e609fc59d6b4b38265f
Joseph Werle <joseph.werle@gmail.com>
2024-03-05 09:49:02 +0000

    chore(api/internal/streams.js): warn for 'bytes' type in 'ReadableStream'

--- api/internal/streams.js
+++ api/internal/streams.js
@@ -2,6 +2,9 @@ export class ReadableStream extends globalThis.ReadableStream {
   constructor (options) {
     if (options?.type === 'bytes') {
       delete options?.type
+      console.warn(
+        'ReadableStream: ReadableByteStreamController is not supported yet'
+      )
     }
 
     super(options)
