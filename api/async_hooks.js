782f90a7395821a3136df36a4b1cbb9263f37593
Joseph Werle <joseph.werle@gmail.com>
2024-03-13 12:39:54 +0100

    refactor(api/async_hooks.js): improve exports

--- api/async_hooks.js
+++ api/async_hooks.js
@@ -1,2 +1,21 @@
-import * as exports from './async/hooks.js'
+import {
+  executionAsyncResource,
+  executionAsyncId,
+  triggerAsyncId,
+  createHook
+} from './async/hooks.js'
+
+import { AsyncLocalStorage } from './async/storage.js'
+import { AsyncResource } from './async/resource.js'
+import * as exports from './async_hooks.js'
+
+export {
+  AsyncLocalStorage,
+  AsyncResource,
+  executionAsyncResource,
+  executionAsyncId,
+  triggerAsyncId,
+  createHook
+}
+
 export default exports
