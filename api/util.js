fb925a7c1530c1e67de8def4ff8be78f5d2939f3
Joseph Werle <joseph.werle@gmail.com>
2024-03-13 12:50:09 +0100

    fix(api/util.js): handle empty strings in 'toProperCase'

--- api/util.js
+++ api/util.js
@@ -223,6 +223,7 @@ export function toBuffer (object, encoding = undefined) {
 }
 
 export function toProperCase (string) {
+  if (!string) return ''
   return string[0].toUpperCase() + string.slice(1)
 }
 
