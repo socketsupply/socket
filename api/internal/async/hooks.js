d51f3456aad7472e57eb2fb85fc00b105e0a07d7
Joseph Werle <joseph.werle@gmail.com>
2024-03-13 11:07:50 +0100

    refactor(api/internal/async/hooks.js): introduce 'CoreAsyncResource', improve state

--- api/internal/async/hooks.js
+++ api/internal/async/hooks.js
@@ -1,12 +1,25 @@
-import { Snapshot, Variable } from '../../async/context.js'
 import { wrap as asyncWrap } from '../../async/wrap.js'
+import { Variable } from '../../async/context.js'
+import gc from '../../gc.js'
 
 let currentAsyncResourceId = 1
 
+/**
+ * The default top level async resource ID
+ * @type {number}
+ */
+export const TOP_LEVEL_ASYNC_RESOURCE_ID = 1
+
+/**
+ * The internal async hook state.
+ */
 export const state = {
   defaultExecutionAsyncId: -1
 }
 
+/**
+ * The current async hooks enabled.
+ */
 export const hooks = {
   init: [],
   before: [],
@@ -15,15 +28,172 @@ export const hooks = {
   promiseResolve: []
 }
 
-export class TopLevelAsyncResource {}
-export const topLevelAsyncResource = new TopLevelAsyncResource()
+/**
+ * A base class for the `AsyncResource` class or other higher level async
+ * resource classes.
+ */
+export class CoreAsyncResource {
+  #type = null
+  #destroyed = false
+  #asyncId = getNextAsyncResourceId()
+  #triggerAsyncId = getDefaultExecutionAsyncId()
+  #requireManualDestroy = false
 
-export const asyncResources = new Map()
-export const asyncContextSnapshot = new Snapshot()
-export const asyncContextVariable = new Variable({
-  name: 'async-hooks',
-  defaultValue: topLevelAsyncResource
-})
+  /**
+   * `CoreAsyncResource` class constructor.
+   * @param {string} type
+   * @param {object|number=} [options]
+   */
+  constructor (type, options = null) {
+    if (!type || typeof type !== 'string') {
+      throw new TypeError(
+        `Expecting 'type' to be a string. Received: ${type}`
+      )
+    }
+
+    if (typeof options === 'number') {
+      options = { triggerAsyncId: options }
+    }
+
+    this.#type = type
+
+    if (Number.isFinite(options?.triggerAsyncId) && options.triggerAsyncId > 0) {
+      this.#triggerAsyncId = options.triggerAsyncId
+    } else if (options?.triggerAsyncId !== undefined) {
+      throw new TypeError(
+        // eslint-disable-next-line
+        `Expecting 'options.triggerAsyncId' to be a positive number.` +
+        `Received: ${options.triggerAsyncId}`
+      )
+    }
+
+    if (typeof options?.requireManualDestroy === 'boolean') {
+      this.#requireManualDestroy = options.requireManualDestroy
+    } else if (options?.requireManualDestroy !== undefined) {
+      throw new TypeError(
+        // eslint-disable-next-line
+        `Expecting 'options.requireManualDestroy' to be a boolean.` +
+        `Received: ${options.requireManualDestroy}`
+      )
+    }
+
+    dispatch('init', this.asyncId(), this.type, this.triggerAsyncId(), this)
+
+    if (!this.#requireManualDestroy) {
+      gc.ref(this)
+    }
+  }
+
+  /**
+   * The `CoreAsyncResource` type.
+   * @type {string}
+   */
+  get type () {
+    return this.#type
+  }
+
+  /**
+   * `true` if the `CoreAsyncResource` was destroyed, otherwise `false`. This
+   * value is only set to `true` if `emitDestroy()` was called, likely from
+   * destroying the resource manually.
+   * @type {boolean}
+   */
+  get destroyed () {
+    return this.#destroyed
+  }
+
+  /**
+   * The unique async resource ID.
+   * @return {number}
+   */
+  asyncId () {
+    return this.#asyncId
+  }
+
+  /**
+   * The trigger async resource ID.
+   * @return {number}
+   */
+  triggerAsyncId () {
+    return this.#triggerAsyncId
+  }
+
+  /**
+   * Manually emits destroy hook for the resource.
+   * @return {CoreAsyncResource}
+   */
+  emitDestroy () {
+    if (!this.#destroyed) {
+      this.#destroyed = true
+    }
+
+    dispatch('destroy', this.asyncId(), this.type, this.triggerAsyncId(), this)
+    return this
+  }
+
+  /**
+   * Binds function `fn` with an optional this `thisArg` binding to run
+   * in the execution context of this `CoreAsyncResource`.
+   * @param {function} fn
+   * @param {object=} [thisArg]
+   * @return {function}
+   */
+  bind (fn, thisArg = undefined) {
+    const resource = this
+    let binding = null
+
+    if (thisArg === undefined) {
+      binding = function (...args) {
+        args.unshift(fn, this)
+        return Reflect.apply(resource.runInAsyncScope, resource, args)
+      }
+    } else {
+      binding = resource.runInAsyncScope.bind(this, fn, thisArg)
+    }
+
+    Object.defineProperty(binding, 'length', {
+      __proto__: null,
+      configurable: true,
+      enumerable: false,
+      writable: false,
+      value: fn.length
+    })
+
+    return binding
+  }
+
+  /**
+   * Runs function `fn` in the execution context of this `CoreAsyncResource`.
+   * @param {function} fn
+   * @param {object=} [thisArg]
+   * @param {...any} [args]
+   * @return {any}
+   */
+  runInAsyncScope (fn, thisArg, ...args) {
+    dispatch('before', this.asyncId(), this.type, this.triggerAsyncId(), this)
+    try {
+      return asyncContextVariable.run(this, fn.bind(thisArg), ...args)
+    } finally {
+      dispatch('after', this.asyncId(), this.type, this.triggerAsyncId(), this)
+    }
+  }
+
+  /**
+   * Implements `gc.finalizer` for gc'd resource cleanup.
+   * @return {gc.Finalizer}
+   * @ignore
+   */
+  [gc.finalizer] () {
+    return {
+      args: [this.asyncId(), this.type, this.triggerAsyncId()],
+      handle (asyncId, type, triggerAsyncId) {
+        dispatch('destroy', asyncId, type, triggerAsyncId)
+      }
+    }
+  }
+}
+
+export class TopLevelAsyncResource extends CoreAsyncResource {}
 
 export function dispatch (hook, asyncId, type, triggerAsyncId, resource) {
   if (hook in hooks) {
@@ -47,12 +217,12 @@ export function executionAsyncResource () {
 }
 
 export function executionAsyncId () {
-  const resource = asyncResources.get(executionAsyncResource())
+  const resource = executionAsyncResource()
   if (resource && typeof resource.asyncId === 'function') {
     return resource.asyncId()
   }
 
-  return 1 // top level
+  return TOP_LEVEL_ASYNC_RESOURCE_ID
 }
 
 export function triggerAsyncId () {
@@ -64,7 +234,7 @@ export function triggerAsyncId () {
     }
   }
 
-  return 1 // top level
+  return TOP_LEVEL_ASYNC_RESOURCE_ID
 }
 
 export function getDefaultExecutionAsyncId () {
@@ -94,5 +264,9 @@ export function wrap (
     }
   }
 }
+
+export const asyncContextVariable = new Variable({ name: 'async-hooks' })
+export const topLevelAsyncResource = new TopLevelAsyncResource('TopLevel')
+asyncContextVariable.defaultValue = topLevelAsyncResource
 
 export default hooks
