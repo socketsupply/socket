6783f4137b95cfe6e626437dbd00e86876e17655
heapwolf <paolo@socketsupply.co>
2024-03-16 20:44:54 +0100

    fix(network): ensure compare is made against buffer, fix incorrect reference

--- api/stream-relay/index.js
+++ api/stream-relay/index.js
@@ -1043,7 +1043,7 @@ export class Peer {
     for (const packet of packets) {
       this.cacheInsert(packet)
 
-      if (this.onPacket) this.onPacket(p, this.port, this.address, true)
+      if (this.onPacket) this.onPacket(packet, this.port, this.address, true)
 
       this.unpublished[packet.packetId.toString('hex')] = Date.now()
       if (globalThis.navigator && !globalThis.navigator.onLine) continue
