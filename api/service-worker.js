d097ecc704b2d2a37c404918b5ec82e392578d4e
Joseph Werle <joseph.werle@gmail.com>
2024-03-26 01:22:08 -0400

    refactor(api/service-worker.js): introduce high level export

---
+++ api/service-worker.js
@@ -0,0 +1,20 @@
+import { ExtendableEvent, FetchEvent } from './service-worker/events.js'
+import { Environment } from './service-worker/env.js'
+import { Database } from './service-worker/database.js'
+import { Context } from './service-worker/context.js'
+
+export {
+  ExtendableEvent,
+  FetchEvent,
+  Environment,
+  Database,
+  Context
+}
+
+export default {
+  ExtendableEvent,
+  FetchEvent,
+  Environment,
+  Database,
+  Context
+}
