873bdc083e94a9c5e72acfcaad8ef627050ac7d3
Joseph Werle <joseph.werle@gmail.com>
2024-03-28 00:37:49 -0400

    refactor(api/commonjs/builtins.js): use shallow copy of builtin exports

--- api/commonjs/builtins.js
+++ api/commonjs/builtins.js
@@ -12,117 +12,121 @@ import _async, {
 } from '../async.js'
 
 // eslint-disable-next-line
-import application from '../application.js'
-import assert from '../assert.js'
+import * as application from '../application.js'
+import * as assert from '../assert.js'
 import * as buffer from '../buffer.js'
 // eslint-disable-next-line
-import child_process from '../child_process.js'
-import console from '../console.js'
-import constants from '../constants.js'
-import crypto from '../crypto.js'
-import dgram from '../dgram.js'
-import dns from '../dns.js'
-import events from '../events.js'
-import extension from '../extension.js'
-import fs from '../fs.js'
-import gc from '../gc.js'
-import http from '../http.js'
-import https from '../https.js'
-import ipc from '../ipc.js'
-import language from '../language.js'
-import location from '../location.js'
-import mime from '../mime.js'
-import network from '../network.js'
-import os from '../os.js'
+import * as child_process from '../child_process.js'
+import * as console from '../console.js'
+import * as constants from '../constants.js'
+import * as crypto from '../crypto.js'
+import * as dgram from '../dgram.js'
+import * as dns from '../dns.js'
+import * as events from '../events.js'
+import * as extension from '../extension.js'
+import * as fs from '../fs.js'
+import * as gc from '../gc.js'
+import * as http from '../http.js'
+import * as https from '../https.js'
+import * as ipc from '../ipc.js'
+import * as language from '../language.js'
+import * as location from '../location.js'
+import * as mime from '../mime.js'
+import * as network from '../network.js'
+import * as os from '../os.js'
 import { posix as path } from '../path.js'
 import process from '../process.js'
-import querystring from '../querystring.js'
-import stream from '../stream.js'
+import * as querystring from '../querystring.js'
+import * as stream from '../stream.js'
 // eslint-disable-next-line
-import string_decoder from '../string_decoder.js'
-import test from '../test.js'
-import timers from '../timers.js'
-import url from '../url.js'
-import util from '../util.js'
-import vm from '../vm.js'
-import window from '../window.js'
+import * as string_decoder from '../string_decoder.js'
+import * as test from '../test.js'
+import * as timers from '../timers.js'
+import * as tty from '../tty.js'
+import * as url from '../url.js'
+import * as util from '../util.js'
+import * as vm from '../vm.js'
+import * as window from '../window.js'
 // eslint-disable-next-line
-import worker_threads from '../worker_threads.js'
+import * as worker_threads from '../worker_threads.js'
 
 /**
  * A mapping of builtin modules
  * @type {object}
  */
-export const builtins = {
-  // eslint-disable-next-line
-  'async': _async,
-  // eslint-disable-next-line
-  async_context: {
-    AsyncLocalStorage,
-    AsyncResource
-  },
-  // eslint-disable-next-line
-  async_hooks: {
-    AsyncLocalStorage,
-    AsyncResource,
-    executionAsyncResource,
-    executionAsyncId,
-    triggerAsyncId,
-    createHook,
-    AsyncHook
-  },
-  application,
-  assert,
-  buffer,
-  console,
-  constants,
-  // eslint-disable-next-line
-  child_process,
-  crypto,
-  dgram,
-  dns,
-  'dns/promises': dns.promises,
-  events,
-  extension,
-  fs,
-  'fs/promises': fs.promises,
-  http,
-  https,
-  gc,
-  ipc,
-  language,
-  location,
-  mime,
-  net: {},
-  network,
-  os,
-  path,
-  // eslint-disable-next-line
-  perf_hooks: {
-    performance: globalThis.performance
-  },
-  process,
-  querystring,
-  stream,
-  'stream/web': stream.web,
-  // eslint-disable-next-line
-  string_decoder,
-  sys: util,
-  test,
-  timers,
-  'timers/promises': timers.promises,
-  tty: {
-    isatty: () => false,
-    WriteStream: util.IllegalConstructor,
-    ReadStream: util.IllegalConstructor
-  },
-  util,
-  url,
-  vm,
-  window,
-  // eslint-disable-next-line
-  worker_threads
+export const builtins = {}
+
+/**
+ * Defines a builtin module by name making a shallow copy of the
+ * module exports.
+ * @param {string}
+ * @param {object} exports
+ */
+export function define (name, exports) {
+  builtins[name] = { ...exports }
 }
+
+// eslint-disable-next-line
+define('async', _async)
+define('async_context', {
+  AsyncLocalStorage,
+  AsyncResource
+})
+define('async_hooks', {
+  AsyncLocalStorage,
+  AsyncResource,
+  executionAsyncResource,
+  executionAsyncId,
+  triggerAsyncId,
+  createHook,
+  AsyncHook
+})
+
+define('application', application)
+define('assert', assert)
+define('buffer', buffer)
+define('console', console)
+define('constants', constants)
+// eslint-disable-next-line
+define('child_process', child_process)
+define('crypto', crypto)
+define('dgram', dgram)
+define('dns', dns)
+define('dns/promises', dns.promises)
+define('events', events)
+define('extension', extension)
+define('fs', fs)
+define('fs/promises', fs.promises)
+define('http', http)
+define('https', https)
+define('gc', gc)
+define('ipc', ipc)
+define('language', language)
+define('location', location)
+define('mime', mime)
+define('net', {})
+define('network', network)
+define('os', os)
+define('path', path)
+define('perf_hooks', { performance: globalThis.performance })
+define('process', process)
+define('querystring', querystring)
+define('stream', stream)
+define('stream/web', stream.web)
+// eslint-disable-next-line
+define('string_decoder', string_decoder)
+define('sys', util)
+define('test', test)
+define('timers', timers)
+define('timers/promises', timers.promises)
+define('tty', tty)
+define('util', util)
+define('url', url)
+define('v8', {})
+define('vm', vm)
+define('window', window)
+// eslint-disable-next-line
+define('worker_threads', worker_threads)
 
 /**
  * Known runtime specific builtin modules.
