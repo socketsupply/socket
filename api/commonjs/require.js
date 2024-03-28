ca11288462f710352f40de28acfac646e0bbc16b
Joseph Werle <joseph.werle@gmail.com>
2024-03-28 00:39:38 -0400

    fix(api/commonjs/require.js): fix module resolution, use 'Package'

--- api/commonjs/require.js
+++ api/commonjs/require.js
@@ -49,34 +49,114 @@ export const globalPaths = [
 ]
 
 /**
+ * An object attached to a `require()` function that contains metadata
+ * about the current module context.
+ */
+export class Meta {
+  #referrer = null
+  #url = null
+
+  /**
+   * `Meta` class constructor.
+   * @param {import('./module.js').Module} module
+   */
+  constructor (module) {
+    this.#referrer = (module.parent ?? module.main).id ?? location.origin
+    this.#url = module.id
+  }
+
+  /**
+   * The referrer (parent) of this module.
+   * @type {string}
+   */
+  get referrer () {
+    return this.#referrer
+  }
+
+  /**
+   * The referrer (parent) of this module.
+   * @type {string}
+   */
+  get url () {
+    return this.#url
+  }
+}
+
+/**
  * Factory for creating a `require()` function based on a module context.
  * @param {CreateRequireOptions} options
  * @return {RequireFunction}
  */
 export function createRequire (options) {
   const { builtins, module } = options
-  const { cache, main } = module
+  const { cache, loaders, main } = module
+
+  // non-standard 'require.meta' object
+  const meta = new Meta(module)
 
   Object.assign(resolve, {
     paths
   })
 
   return Object.assign(require, {
+    extensions: loaders,
     resolve,
+    loaders,
     cache,
+    meta,
     main
   })
 
   /**
+   * @param {string} input
+   * @param {ResolveOptions & RequireOptions=} [options
+   * @ignore
+   */
+  function applyResolvers (input, options = null) {
+    const resolvers = Array
+      .from(module.resolvers)
+      .concat(options?.resolvers)
+      .filter(Boolean)
+
+    return next(input)
+
+    function next (specifier) {
+      if (resolvers.length === 0) return specifier
+      const resolver = resolvers.shift()
+      return resolver(specifier, module, next)
+    }
+  }
+
+  /**
    * Requires a module at for a given `input` which can be a relative file,
    * named module, or an absolute URL.
-   * @param {string|URL} athname
+   * @param {string|URL} input
    * @param {RequireOptions=} [options]
-   * @return {string}
+   * @throws ModuleNotFoundError
+   * @throws ReferenceError
+   * @throws SyntaxError
+   * @throws TypeError
+   * @return {any}
    */
   function require (input, options = null) {
+    const resolvedInput = applyResolvers(input, options)
+
+    if (resolvedInput && typeof resolvedInput !== 'string') {
+      return resolvedInput
+    }
+
+    if (resolvedInput.includes('\n') || resolvedInput.length > 256) {
+      return resolvedInput
+    }
+
+    input = resolvedInput
+
     if (isBuiltin(input, { builtins: options?.builtins ?? builtins })) {
       return getBuiltin(input, { builtins: options?.builtins ?? builtins })
+    }
+
+    if (cache[input]) {
+      return cache[input].exports
     }
 
     const resolved = resolve(input, {
@@ -84,86 +164,99 @@ export function createRequire (options) {
       ...options
     })
 
-    if (cache[input]) {
-      return cache[input].exports
-    }
-
-    if (!resolved) {
-      throw new ModuleNotFoundError(
-        `Cannnot find module '${input}`,
-        module.children.map((mod) => mod.id)
-      )
+    if (cache[resolved]) {
+      return cache[resolved].exports
     }
 
     const child = module.createModule(resolved, {
       ...options,
       package: input.startsWith('.') || input.startsWith('/')
         ? module.package
-        : Package.find(resolved, {
+        : new Package(resolved, {
           loader: new Loader(module.loader),
           ...options
         })
     })
 
+    cache[resolved] = child
+    cache[input] = child
+
     if (child.load(options)) {
-      cache[resolved] = child
       return child.exports
     }
+
+    throw new ModuleNotFoundError(
+      `Cannnot find module '${input}'`,
+      module.children.map((mod) => mod.id)
+    )
   }
 
   /**
    * Resolve a module `input` to an absolute URL.
    * @param {string|URL} pathname
    * @param {ResolveOptions=} [options]
+   * @throws ModuleNotFoundError
    * @return {string}
    */
   function resolve (input, options = null) {
-    if (isBuiltin(input, builtins)) {
+    if (input instanceof URL) {
+      input = String(input)
+    }
+
+    const resolvedInput = applyResolvers(input, options)
+
+    if (resolvedInput && typeof resolvedInput !== 'string') {
       return input
     }
 
-    const url = URL.canParse(input)
-      ? new URL(input)
-      : null
-
-    const origins = url !== null
-      ? [url.origin]
-      : resolve.paths(input)
-
-    if (url !== null) {
-      input = `.${url.pathname + url.search}`
+    if (resolvedInput.includes('\n') || resolvedInput.length > 256) {
+      return input
     }
 
+    input = resolvedInput
+
+    if (isBuiltin(input, { builtins: options?.builtins ?? builtins })) {
+      return input
+    }
+
+    // A URL was given, try to resolve it as a package
+    if (URL.canParse(input)) {
+      return module.package.resolve(input, {
+        type: module.package.type,
+        ...options
+      })
+    }
+
+    const origins = resolve.paths(input)
+
     for (const origin of origins) {
-      if (url) {
-        const status = module.loader.status(input, origin, options)
-
-        if (status.ok) {
-          return status.id
-        }
-      } else if (input.startsWith('.') || input.startsWith('/')) {
+      // relative require
+      if (input.startsWith('.') || input.startsWith('/')) {
         return module.resolve(input)
-      } else {
-        const components = input.split('/')
-        const manifest = new Package(components.shift(), {
-          prefix: '',
-          loader: new Loader(origin, module.loader)
+      } else { // named module
+        const moduleName = Package.Name.from(input)
+        const pathname = moduleName.pathname.replace(moduleName.name, '.')
+        const manifest = new Package(moduleName.name, {
+          loader: new Loader(origin)
         })
 
-        const loaded = manifest.load({
-          type: module.package.type,
-          ...options
-        })
-
-        if (loaded) {
-          const pathname = components.join('/') || '.'
+        try {
           return manifest.resolve(pathname, {
             type: module.package.type,
             ...options
           })
+        } catch (err) {
+          if (err.code !== 'MODULE_NOT_FOUND') {
+            throw err
+          }
         }
       }
     }
+
+    throw new ModuleNotFoundError(
+      `Cannnot find module '${input}'`,
+      module.children.map((mod) => mod.id)
+    )
   }
 
   /**
@@ -198,10 +291,15 @@ export function createRequire (options) {
       origin = new URL('..', origin).href
     }
 
-    return Array
+    const results = Array
       .from(origins)
-      .map((origin) => new URL(options.prefix, origin))
+      .map((origin) => origin.endsWith(options.prefix)
+        ? new URL(origin)
+        : new URL(options.prefix, origin)
+      )
       .map((url) => url.href)
+
+    return Array.from(new Set(results))
   }
 }
 
