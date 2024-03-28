9995fd57c2ffffc630aadbbd53dd11499effe00f
Joseph Werle <joseph.werle@gmail.com>
2024-03-28 00:33:46 -0400

    feat(api/internal/post-message.js): implement 'Symbol.serialize' mapper

---
+++ api/internal/post-message.js
@@ -0,0 +1,53 @@
+const platform = {
+  BroadcastChannelPostMessage: globalThis.BroadcastChannel.prototype.postMessage,
+  MessageChannelPostMessage: globalThis.MessageChannel.prototype.postMessage,
+  GlobalPostMessage: globalThis.postMessage
+}
+
+globalThis.BroadcastChannel.prototype.postMessage = function (message, ...args) {
+  message = handlePostMessage(message)
+  return platform.BroadcastChannelPostMessage.call(this, message, ...args)
+}
+
+globalThis.MessageChannel.prototype.postMessage = function (message, ...args) {
+  return platform.MessageChannelPostMessage.call(this, handlePostMessage(message), ...args)
+}
+
+globalThis.postMessage = function (message, ...args) {
+  return platform.GlobalPostMessage.call(this, handlePostMessage(message), ...args)
+}
+
+function handlePostMessage (message) {
+  if (!message || typeof message !== 'object') {
+    return message
+  }
+
+  return map(message, (value) => {
+    if (typeof value[Symbol.serialize] !== 'function') {
+      return value
+    }
+
+    return value[Symbol.serialize]()
+  })
+}
+
+function map (object, callback) {
+  if (Array.isArray(object)) {
+    for (let i = 0; i < object.length; ++i) {
+      object[i] = map(object[i], callback)
+    }
+  } else if (object && typeof object === 'object') {
+    object = callback(object)
+    for (const key in object) {
+      object[key] = map(object[key], callback)
+    }
+  }
+
+  if (object && typeof object === 'object') {
+    return callback(object)
+  } else {
+    return object
+  }
+}
+
+export default null
