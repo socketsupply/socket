b601d86cd23a73837ab214506ba2f6d2ac61886b
Joseph Werle <joseph.werle@gmail.com>
2024-03-28 00:36:44 -0400

    refactor(api/internal/symbols.js): export 'Symbol.serialize'

--- api/internal/symbols.js
+++ api/internal/symbols.js
@@ -1,5 +1,7 @@
-export const dispose = Symbol.dispose ?? Symbol.for('dispose')
+export const dispose = Symbol.dispose ?? Symbol.for('socket.runtime.gc.finalizer')
+export const serialize = Symbol.serialize ?? Symbol.for('socket.runtime.serialize')
 
 export default {
-  dispose
+  dispose,
+  serialize
 }
