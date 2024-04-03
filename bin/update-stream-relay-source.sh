c1ca441785e9d8e7745e03f898beff72a23746bf
Peter Johnson <insomnia@rcpt.at>
2024-04-03 17:16:34 +0200

    ci: update-stream-relay-source.sh
    Updated `sed` to be portable between OSX & Linux

--- bin/update-stream-relay-source.sh
+++ bin/update-stream-relay-source.sh
@@ -8,7 +8,7 @@ cp -rf node_modules/@socketsupply/stream-relay/src api/stream-relay || exit $?
 rm -rf node_modules/@socketsupply/{socket,socket-{darwin,linux,win32}*,stream-relay} || exit $?
 
 for file in $(find api/stream-relay -type f); do
-  sed -i '' -e "s/'socket:\(.*\)'/'..\/\1.js'/g" "$file" || exit $?
+  sed -i.bak -e "s/'socket:\(.*\)'/'..\/\1.js'/g" "$file" && rm "$file.bak" || exit $?
 done
 
 {
