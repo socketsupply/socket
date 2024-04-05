db355aafdc41fd7518b350ae4639def3f657918e
Joseph Werle <joseph.werle@gmail.com>
2024-04-05 15:49:54 -0400

    refactor(api/internal/primitives.js): install web streams into env:

--- api/internal/streams.js
+++ api/internal/streams.js
@@ -1,86 +1,39 @@
-import { Deferred } from '../async/deferred.js'
+import {
+  ReadableStream,
+  ReadableStreamBYOBReader,
+  ReadableByteStreamController,
+  ReadableStreamBYOBRequest,
+  ReadableStreamDefaultController,
+  ReadableStreamDefaultReader,
 
-const PlatformReadableStreamBYOBReader = globalThis.ReadableStreamBYOBReader
-const PlatformReadableStreamBYOBRequest = globalThis.ReadableStreamBYOBRequest
-const PlatformReadableByteStreamController = globalThis.ReadableByteStreamController
+  WritableStream,
+  WritableStreamDefaultController,
+  WritableStreamDefaultWriter,
 
-export class ReadableStream extends globalThis.ReadableStream {
-  constructor (options) {
-    if (options?.type === 'bytes') {
-      delete options?.type
-      console.warn(
-        'ReadableStream: ReadableByteStreamController is not supported yet'
-      )
-    }
+  TransformStream,
+  TransformStreamDefaultController,
 
-    super(options)
-  }
+  ByteLengthQueuingStrategy,
+  CountQueuingStrategy
+} from './streams/web.js'
 
-  getReader (options) {
-    if (options?.mode === 'byob') {
-      return new ReadableStreamBYOBReader(this)
-    }
+export {
+  ReadableStream,
+  ReadableStreamBYOBReader,
+  ReadableByteStreamController,
+  ReadableStreamBYOBRequest,
+  ReadableStreamDefaultController,
+  ReadableStreamDefaultReader,
 
-    return super.getReader(options)
-  }
+  WritableStream,
+  WritableStreamDefaultController,
+  WritableStreamDefaultWriter,
+
+  TransformStream,
+  TransformStreamDefaultController,
+
+  ByteLengthQueuingStrategy,
+  CountQueuingStrategy
 }
 
-class ReadableStreamBYOBReaderCancellation {
-  reason = undefined
-  state = false
-}
-
-class ReadableStreamBYOBReadResult {
-  value = undefined
-  done = false
-
-  constructor (value = undefined, done = false) {
-    this.value = value
-    this.done = done
-  }
-}
-
-export const ReadableStreamBYOBReader = PlatformReadableStreamBYOBReader || class ReadableStreamBYOBReader {
-  #closed = new Deferred()
-  #cancellation = new ReadableStreamBYOBReaderCancellation()
-
-  get closed () {
-    return this.#closed.promise
-  }
-
-  cancel (reason = undefined) {
-    this.#cancellation.state = true
-    if (typeof reason === 'string') {
-      this.#cancellation.reason = reason
-    }
-    this.#closed.resolve(reason)
-    return this.#closed.promise
-  }
-
-  read (view) {
-    if (this.#cancellation.state === true) {
-      return new ReadableStreamBYOBReadResult(undefined, true)
-    }
-  }
-}
-
-export const ReadableStreamBYOBRequest = PlatformReadableStreamBYOBRequest || class ReadableStreamBYOBRequest {
-  #view = null
-
-  get view () {
-    return this.#view
-  }
-
-  respond (bytesWritten) {
-  }
-
-  respondWithNewView (view) {
-  }
-}
-
-export const ReadableByteStreamController = PlatformReadableByteStreamController || class ReadableByteStreamController {
-}
-
-export default {
-  ReadableStream
-}
+export default null
