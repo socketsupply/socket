ede9749aede88ff11b781e1da0866ba4383b676b
Joseph Werle <joseph.werle@gmail.com>
2024-04-05 15:51:13 -0400

    refactor(api/stream/web.js): import web streams

--- api/stream/web.js
+++ api/stream/web.js
@@ -1,20 +1,45 @@
+import {
+  ReadableStream,
+  ReadableStreamBYOBReader,
+  ReadableByteStreamController,
+  ReadableStreamBYOBRequest,
+  ReadableStreamDefaultController,
+  ReadableStreamDefaultReader,
+
+  WritableStream,
+  WritableStreamDefaultController,
+  WritableStreamDefaultWriter,
+
+  TransformStream,
+  TransformStreamDefaultController,
+
+  ByteLengthQueuingStrategy,
+  CountQueuingStrategy
+} from '../internal/streams.js'
+
 import * as exports from './web.js'
 
 class UnsupportedStreamInterface {}
 
-export const ReadableStream = globalThis.ReadableStream ?? UnsupportedStreamInterface
-export const ReadableStreamDefaultReader = globalThis.ReadableStreamDefaultReader ?? UnsupportedStreamInterface
-export const ReadableStreamBYOBReader = globalThis.ReadableStreamBYOBReader ?? UnsupportedStreamInterface
-export const ReadableStreamBYOBRequest = globalThis.ReadableStreamBYOBRequest ?? UnsupportedStreamInterface
-export const ReadableByteStreamController = globalThis.ReadableByteStreamController ?? UnsupportedStreamInterface
-export const ReadableStreamDefaultController = globalThis.ReadableStreamDefaultController ?? UnsupportedStreamInterface
-export const TransformStream = globalThis.TransformStream ?? UnsupportedStreamInterface
-export const TransformStreamDefaultController = globalThis.TransformStreamDefaultController ?? UnsupportedStreamInterface
-export const WritableStream = globalThis.WritableStream ?? UnsupportedStreamInterface
-export const WritableStreamDefaultWriter = globalThis.WritableStreamDefaultWriter ?? UnsupportedStreamInterface
-export const WritableStreamDefaultController = globalThis.WritableStreamDefaultController ?? UnsupportedStreamInterface
-export const ByteLengthQueuingStrategy = globalThis.ByteLengthQueuingStrategy ?? UnsupportedStreamInterface
-export const CountQueuingStrategy = globalThis.CountQueuingStrategy ?? UnsupportedStreamInterface
+export {
+  ReadableStream,
+  ReadableStreamBYOBReader,
+  ReadableByteStreamController,
+  ReadableStreamBYOBRequest,
+  ReadableStreamDefaultController,
+  ReadableStreamDefaultReader,
+
+  WritableStream,
+  WritableStreamDefaultController,
+  WritableStreamDefaultWriter,
+
+  TransformStream,
+  TransformStreamDefaultController,
+
+  ByteLengthQueuingStrategy,
+  CountQueuingStrategy
+}
+
 export const TextEncoderStream = globalThis.TextEncoderStream ?? UnsupportedStreamInterface
 export const TextDecoderStream = globalThis.TextDecoderStream ?? UnsupportedStreamInterface
 export const CompressionStream = globalThis.CompressionStream ?? UnsupportedStreamInterface
