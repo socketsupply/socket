a3614a74f86a93cce5921f29b3eee4d6ec7d8561
Joseph Werle <joseph.werle@gmail.com>
2024-04-09 18:39:59 -0400

    feat(api/internal/serialize.js): initial 'serialize' internal module

---
+++ api/internal/serialize.js
@@ -0,0 +1,39 @@
+export default function serialize (value) {
+  if (!value || typeof value !== 'object') {
+    return value
+  }
+
+  return map(value, (value) => {
+    if (typeof value[Symbol.serialize] === 'function') {
+      return value[Symbol.serialize]()
+    }
+
+    if (typeof value.toJSON === 'function') {
+      return value.toJSON()
+    }
+
+    return value
+  })
+}
+
+function map (object, callback) {
+  if (Array.isArray(object)) {
+    for (let i = 0; i < object.length; ++i) {
+      object[i] = map(object[i], callback)
+    }
+  } else if (object && typeof object === 'object') {
+    object = callback(object)
+    for (const key in object) {
+      const descriptor = Object.getOwnPropertyDescriptor(object, key)
+      if (descriptor && descriptor.writable) {
+        object[key] = map(object[key], callback)
+      }
+    }
+  }
+
+  if (object && typeof object === 'object') {
+    return callback(object)
+  } else {
+    return object
+  }
+}
