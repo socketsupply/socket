f2f95ad9e443b5934e817cba4fdd3d74d9f4040c
Joseph Werle <joseph.werle@gmail.com>
2024-04-09 18:36:34 -0400

    refactor(api/module.js): improve exports

--- api/module.js
+++ api/module.js
@@ -4,8 +4,15 @@
 import { createRequire, Module } from './commonjs/module.js'
 import builtins, { isBuiltin } from './commonjs/builtins.js'
 
-export { createRequire, Module, builtins, isBuiltin }
+/**
+ * @typedef {import('./commonjs/module.js').ModuleOptions} ModuleOptions
+ * @typedef {import('./commonjs/module.js').ModuleResolver} ModuleResolver
+ * @typedef {import('./commonjs/module.js').ModuleLoadOptions} ModuleLoadOptions
+ * @typedef {import('./commonjs/module.js').RequireFunction} RequireFunction
+ * @typedef {import('./commonjs/module.js').CreateRequireOptions} CreateRequireOptions
+ */
 
+export { createRequire, Module, builtins, isBuiltin }
 export const builtinModules = builtins
 
 export default Module
