3a62d72259b89cc6a6d82818d2b124ef904a8b3e
Joseph Werle <joseph.werle@gmail.com>
2024-04-09 18:41:11 -0400

    refactor(api/internal/post-message.js): use 'serialize' internal module

--- api/internal/post-message.js
+++ api/internal/post-message.js
@@ -1,3 +1,5 @@
+import serialize from './serialize.js'
+
 const platform = {
   BroadcastChannelPostMessage: globalThis.BroadcastChannel.prototype.postMessage,
   MessageChannelPostMessage: globalThis.MessageChannel.prototype.postMessage,
@@ -18,39 +20,7 @@ globalThis.postMessage = function (message, ...args) {
 }
 
 function handlePostMessage (message) {
-  if (!message || typeof message !== 'object') {
-    return message
-  }
-
-  return map(message, (value) => {
-    if (typeof value[Symbol.serialize] !== 'function') {
-      return value
-    }
-
-    return value[Symbol.serialize]()
-  })
-}
-
-function map (object, callback) {
-  if (Array.isArray(object)) {
-    for (let i = 0; i < object.length; ++i) {
-      object[i] = map(object[i], callback)
-    }
-  } else if (object && typeof object === 'object') {
-    object = callback(object)
-    for (const key in object) {
-      const descriptor = Object.getOwnPropertyDescriptor(object, key)
-      if (descriptor && descriptor.writable) {
-        object[key] = map(object[key], callback)
-      }
-    }
-  }
-
-  if (object && typeof object === 'object') {
-    return callback(object)
-  } else {
-    return object
-  }
+  return serialize(message)
 }
 
 export default null
