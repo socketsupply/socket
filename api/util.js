e73f528d3056a4462245f907238678ce96a7f7da
Joseph Werle <joseph.werle@gmail.com>
2024-06-05 13:43:44 +0200

    fix(api/util.js): fix ESM detection regex

--- api/util.js
+++ api/util.js
@@ -989,13 +989,15 @@ export function inherits (Constructor, Super) {
   Object.setPrototypeOf(Constructor.prototype, Super.prototype)
 }
 
+export const ESM_TEST_REGEX = /\b(import\s*[\w{},*\s]*\s*from\s*['"][^'"]+['"]|export\s+(?:\*\s*from\s*['"][^'"]+['"]|default\s*from\s*['"][^'"]+['"]|[\w{}*\s,]+))\s*(?:;|\b)/
+
 /**
  * @ignore
  * @param {string} source
  * @return {boolean}
  */
 export function isESMSource (source) {
-  if (/\b(import\s*[\w{},*\s]*\s*from\s*['"][^'"]+['"]|export\s*(?:\*\s*from\s*['"][^'"]+['"]|default\s*from\s*['"][^'"]+['"]|[\w{}*\s,]+))\s*(?:;|\b)/.test(source)) {
+  if (ESM_TEST_REGEX.test(source)) {
     return true
   }
 
