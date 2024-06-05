1f2cd6bcce03a36aa42e21391cf090f6ef260e04
Joseph Werle <joseph.werle@gmail.com>
2024-06-05 13:45:40 +0200

    fix(api/internal): fix document title to window title proxy

--- api/internal/primitives.js
+++ api/internal/primitives.js
@@ -406,23 +406,25 @@ export function init () {
     // create <title> tag in document if it doesn't exist
     globalThis.document.title ||= ''
     // initial value
-    globalThis.document.addEventListener('DOMContentLoaded', () => {
-      const title = globalThis.document.title
-      if (title.length !== 0) {
-        const index = globalThis.__args.index
-        const o = new URLSearchParams({ value: title, index }).toString()
-        ipc.postMessage(`ipc://window.setTitle?${o}`)
+    globalThis.document.addEventListener('DOMContentLoaded', async () => {
+      const { title } = globalThis.document
+      if (title) {
+        const result = await ipc.request('window.setTitle', {
+          targetWindowIndex: globalThis.__args.index,
+          value: title
+        })
       }
     })
 
     // globalThis.document is unconfigurable property so we need to use MutationObserver here
-    const observer = new MutationObserver((mutationList) => {
+    const observer = new MutationObserver(async (mutationList) => {
       for (const mutation of mutationList) {
         if (mutation.type === 'childList') {
-          const index = globalThis.__args.index
           const title = mutation.addedNodes[0].textContent
-          const o = new URLSearchParams({ value: title, index }).toString()
-          ipc.postMessage(`ipc://window.setTitle?${o}`)
+          const result = await ipc.request('window.setTitle', {
+            targetWindowIndex: globalThis.__args.index,
+            value: title
+          })
         }
       }
     })
