ccaa9ed454920ab6ba1299c3c1a0b5318dd5393a
Joseph Werle <joseph.werle@gmail.com>
2024-07-18 11:01:35 +0100

    fix(core/modules/platform): fix 'CorePlatform::openExternal'

--- src/core/modules/platform.cc
+++ src/core/modules/platform.cc
@@ -161,125 +161,126 @@ namespace SSC {
     const String& value,
     const CoreModule::Callback& callback
   ) const {
-    #if SOCKET_RUNTIME_PLATFORM_APPLE
-      const auto url = [NSURL URLWithString: @(value.c_str())];
+  #if SOCKET_RUNTIME_PLATFORM_APPLE
+    this->core->dispatchEventLoop([=]() {
+      __block const auto url = [NSURL URLWithString: @(value.c_str())];
 
-      #if SOCKET_RUNTIME_PLATFORM_IOS
-        [UIApplication.sharedApplication openURL: url options: @{} completionHandler: ^(BOOL success) {
-          auto json = JSON::Object {};
+    #if SOCKET_RUNTIME_PLATFORM_IOS
+      [UIApplication.sharedApplication openURL: url options: @{} completionHandler: ^(BOOL success) {
+        JSON::Object json;
+        if (!success) {
+          json = JSON::Object::Entries {
+            {"source", "platform.openExternal"},
+            {"err", JSON::Object::Entries {
+              {"message", "Failed to open external URL"}
+            }}
+          };
+        } else {
+          json = JSON::Object::Entries {
+            {"source", "platform.openExternal"},
+            {"data", JSON::Object::Entries {{"url", url.absoluteString.UTF8String}}}
+          };
+        }
 
-          if (!success) {
+        callback(seq, json, Post{});
+      }];
+    #else
+      auto workspace = [NSWorkspace sharedWorkspace];
+      auto configuration = [NSWorkspaceOpenConfiguration configuration];
+      [workspace openURL: url
+           configuration: configuration
+       completionHandler: ^(NSRunningApplication *app, NSError *error)
+      {
+        JSON::Object json;
+         if (error) {
+           if (error.debugDescription.UTF8String) {
+             debug("%s", error.debugDescription.UTF8String);
+           }
+
+           json = JSON::Object::Entries {
+             {"source", "platform.openExternal"},
+             {"err", JSON::Object::Entries {
+               {"message", error.localizedDescription.UTF8String}
+             }}
+           };
+         } else {
+          json = JSON::Object::Entries {
+            {"source", "platform.openExternal"},
+            {"data", JSON::Object::Entries {{ "url", url.absoluteString.UTF8String}}}
+          };
+         }
+
+         callback(seq, json, Post{});
+      }];
+    #endif
+    });
+  #elif SOCKET_RUNTIME_PLATFORM_LINUX
+    auto list = gtk_window_list_toplevels();
+    auto json = JSON::Object {};
+
+    // initial state is a failure
+    json = JSON::Object::Entries {
+      {"source", "platform.openExternal"},
+      {"err", JSON::Object::Entries {
+        {"message", "Failed to open external URL"}
+      }}
+    };
+
+    if (list != nullptr) {
+      for (auto entry = list; entry != nullptr; entry = entry->next) {
+        auto window = GTK_WINDOW(entry->data);
+
+        if (window != nullptr && gtk_window_is_active(window)) {
+          auto err = (GError*) nullptr;
+          auto uri = value.c_str();
+          auto ts = GDK_CURRENT_TIME;
+
+          /**
+           * GTK may print a error in the terminal that looks like:
+           *
+           *   libva error: vaGetDriverNameByIndex() failed with unknown libva error, driver_name = (null)
+           *
+           * It doesn't prevent the URI from being opened.
+           * See https://github.com/intel/media-driver/issues/1349 for more info
+           */
+          auto success = gtk_show_uri_on_window(window, uri, ts, &err);
+
+          if (success) {
+            json = JSON::Object::Entries {
+              {"source", "platform.openExternal"},
+              {"data", JSON::Object::Entries {}}
+            };
+          } else if (err != nullptr) {
             json = JSON::Object::Entries {
               {"source", "platform.openExternal"},
               {"err", JSON::Object::Entries {
-                {"message", "Failed to open external URL"}
+                {"message", err->message}
               }}
             };
-          } else {
-            json = JSON::Object::Entries {
-              {"source", "platform.openExternal"},
-              {"data", JSON::Object::Entries {{"url", value}}}
-            };
           }
 
-          callback(seq, json, Post{});
-        }];
-      #else
-        auto workspace = [NSWorkspace sharedWorkspace];
-        auto configuration = [NSWorkspaceOpenConfiguration configuration];
-        [workspace openURL: url
-             configuration: configuration
-         completionHandler: ^(NSRunningApplication *app, NSError *error)
-        {
-           auto json = JSON::Object {};
-           if (error) {
-             if (error.debugDescription.UTF8String) {
-               debug("%s", error.debugDescription.UTF8String);
-             }
-
-             json = JSON::Object::Entries {
-               {"source", "platform.openExternal"},
-               {"err", JSON::Object::Entries {
-                 {"message", error.localizedDescription.UTF8String}
-               }}
-             };
-           } else {
-            json = JSON::Object::Entries {
-              {"source", "platform.openExternal"},
-              {"data", JSON::Object::Entries {{ "url", value}}}
-            };
-           }
-
-          callback(seq, json, Post{});
-        }];
-      #endif
-    #elif SOCKET_RUNTIME_PLATFORM_LINUX
-      auto list = gtk_window_list_toplevels();
-      auto json = JSON::Object {};
-
-      // initial state is a failure
-      json = JSON::Object::Entries {
-        {"source", "platform.openExternal"},
-        {"err", JSON::Object::Entries {
-          {"message", "Failed to open external URL"}
-        }}
-      };
-
-      if (list != nullptr) {
-        for (auto entry = list; entry != nullptr; entry = entry->next) {
-          auto window = GTK_WINDOW(entry->data);
-
-          if (window != nullptr && gtk_window_is_active(window)) {
-            auto err = (GError*) nullptr;
-            auto uri = value.c_str();
-            auto ts = GDK_CURRENT_TIME;
-
-            /**
-             * GTK may print a error in the terminal that looks like:
-             *
-             *   libva error: vaGetDriverNameByIndex() failed with unknown libva error, driver_name = (null)
-             *
-             * It doesn't prevent the URI from being opened.
-             * See https://github.com/intel/media-driver/issues/1349 for more info
-             */
-            auto success = gtk_show_uri_on_window(window, uri, ts, &err);
-
-            if (success) {
-              json = JSON::Object::Entries {
-                {"source", "platform.openExternal"},
-                {"data", JSON::Object::Entries {}}
-              };
-            } else if (err != nullptr) {
-              json = JSON::Object::Entries {
-                {"source", "platform.openExternal"},
-                {"err", JSON::Object::Entries {
-                  {"message", err->message}
-                }}
-              };
-            }
-
-            break;
-          }
+          break;
         }
-
-        g_list_free(list);
       }
 
-      callback(seq, json, Post{});
-    #elif SOCKET_RUNTIME_PLATFORM_WINDOWS
-      auto uri = value.c_str();
-      ShellExecute(nullptr, "Open", uri, nullptr, nullptr, SW_SHOWNORMAL);
-      // TODO how to detect success here. do we care?
-      callback(seq, JSON::Object{}, Post{});
-    #else
-      const auto json = JSON::Object::Entries {
-        {"source", "platform.openExternal"},
-        {"err", JSON::Object::Entries {
-          {"type", "NotSupportedError"},
-          {"message", "Operation not supported"}
-        }}
-      };
-      callback(seq, json, Post{});
-    #endif
+      g_list_free(list);
+    }
+
+    callback(seq, json, Post{});
+  #elif SOCKET_RUNTIME_PLATFORM_WINDOWS
+    auto uri = value.c_str();
+    ShellExecute(nullptr, "Open", uri, nullptr, nullptr, SW_SHOWNORMAL);
+    // TODO how to detect success here. do we care?
+    callback(seq, JSON::Object{}, Post{});
+  #else
+    const auto json = JSON::Object::Entries {
+      {"source", "platform.openExternal"},
+      {"err", JSON::Object::Entries {
+        {"type", "NotSupportedError"},
+        {"message", "Operation not supported"}
+      }}
+    };
+    callback(seq, json, Post{});
+  #endif
   }
 }
