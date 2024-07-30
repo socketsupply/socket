54ef3bf939029d64b0f3c2c4308ae627092572af
Joseph Werle <joseph.werle@gmail.com>
2024-07-30 21:39:59 +0200

    fix(window/apple): fix bad var use

--- src/window/apple.mm
+++ src/window/apple.mm
@@ -824,7 +824,7 @@ namespace SSC {
               double number = 0.0f;
 
               try {
-                number = std::stod(result);
+                number = std::stod(value);
               } catch (...) {
                 callback(value);
                 return;
