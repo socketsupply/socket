6dbb81b56ac20ffba773856c1d96aad97d9da319
Joseph Werle <joseph.werle@gmail.com>
2024-11-04 20:05:35 -0500

    Revert "fix(window/win.cc): handle closable and non-resiable"
    This reverts commit a99f6608a37f3be4f7b512f83e65fd074c40b605.

--- src/window/win.cc
+++ src/window/win.cc
@@ -668,8 +668,6 @@ namespace SSC {
       if (!options.resizable) {
         style &= ~(WS_CAPTION | WS_THICKFRAME | WS_MINIMIZEBOX | WS_MAXIMIZEBOX | WS_SYSMENU);
         style &= ~(WS_EX_DLGMODALFRAME | WS_EX_CLIENTEDGE | WS_EX_STATICEDGE);
-      } else if (options.closable && !options.frameless) {
-        style |= WS_CAPTION | WS_SYSMENU;
       }
 
       this->window = CreateWindowEx(
